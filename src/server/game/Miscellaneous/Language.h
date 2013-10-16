cafa79ef20ab00f95e7886861570b5aeb385174d
Sélénium <francois.milleniumstudio@gmail.com>
2013-10-16 18:17:32 +0200

    Improve .damage command, now can damage destructible gameobjects => ".damage go <guid> <damage>"
    Signed-off-by: Sélénium <francois.milleniumstudio@gmail.com>

--- src/server/game/Miscellaneous/Language.h
+++ src/server/game/Miscellaneous/Language.h
@@ -182,8 +182,10 @@ enum TrinityStrings
     LANG_PHASING_FAILED                 = 179,	
     LANG_PHASING_LAST_PHASE             = 180,	
     LANG_PHASING_LIST                   = 181,	
-    LANG_PHASING_PHASEMASK              = 182,	
-    // Room for more level 1              183-199 not used
+    LANG_PHASING_PHASEMASK              = 182,
+    LANG_INVALID_GAMEOBJECT_TYPE        = 183,              // Damage on GO
+    LANG_GAMEOBJECT_DAMAGED             = 184,
+    // Room for more level 1              185-199 not used
 
     // level 2 chat
     LANG_NO_SELECTION                   = 200,
