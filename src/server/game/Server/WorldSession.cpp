0de4608b6618d2245c8bc3fd4ccf11747d66384e
Tw1nk_1 <nikazhazha@mail.ru>
2013-10-19 02:22:47 +0300

    Compile fix

--- src/server/game/Server/WorldSession.cpp
+++ src/server/game/Server/WorldSession.cpp
@@ -111,7 +111,7 @@ timeCharEnumOpcode(0),
 playerLoginCounter(0),
 sellItemOpcodeTimer(5000),
 sellItemOpcodeCounter(0),
-timeLastServerCommand(0), timeLastArenaTeamCommand(0), timeLastCalendarInvCommand(0), timeLastChangeSubGroupCommand,
+timeLastServerCommand(0), timeLastArenaTeamCommand(0), timeLastCalendarInvCommand(0), timeLastChangeSubGroupCommand(0),
 m_uiAntispamMailSentCount(0), m_uiAntispamMailSentTimer(0)
 {
     _warden = NULL;
