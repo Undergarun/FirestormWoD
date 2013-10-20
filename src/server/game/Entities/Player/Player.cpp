b4b8d611ec0e1abf97e4f0deb5fc1dda880f7b7e
n0n4m3 <n0n4m3nsk@gmail.com>
2013-10-20 19:33:48 +0400

    Fix player report about AFK on BG(not offlike, but works and correct).

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -24237,12 +24237,13 @@ void Player::ReportedAfkBy(Player* reporter)
     if (m_bgData.bgAfkReporter.find(reporter->GetGUIDLow()) == m_bgData.bgAfkReporter.end() && !HasAura(43680) && !HasAura(43681) && reporter->CanReportAfkDueToLimit())
     {
         m_bgData.bgAfkReporter.insert(reporter->GetGUIDLow());
-        // 3 players have to complain to apply debuff
-        if (m_bgData.bgAfkReporter.size() >= 3)
+        // 5 players have to complain to apply debuff
+        if (m_bgData.bgAfkReporter.size() >= 5)
         {
             // cast 'Idle' spell
-            CastSpell(this, 43680, true);
+            //CastSpell(this, 43680, true);
             m_bgData.bgAfkReporter.clear();
+            LeaveBattleground();
         }
     }
 }
