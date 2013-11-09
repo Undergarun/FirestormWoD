f3b43749dcae983dce81d4d98612a9dd2dbb9c65
Tw1nk_1 <nikazhazha@mail.ru>
2013-11-09 12:52:23 +0200

    Fix stack of Fortitude buffs

---
+++ sql/updates/world/2013/11/2013_11_10_world_spell_group.sql
@@ -0,0 +1,6 @@
+INSERT INTO `spell_group` VALUES ('1109','109732');
+INSERT INTO `spell_group` VALUES ('1109','69377');
+INSERT INTO `spell_group` VALUES ('1109','86507');
+INSERT INTO `spell_group` VALUES ('1109','111922');
+INSERT INTO `spell_group` VALUES ('1109','111923');
+INSERT INTO `spell_group` VALUES ('1109','96175');
