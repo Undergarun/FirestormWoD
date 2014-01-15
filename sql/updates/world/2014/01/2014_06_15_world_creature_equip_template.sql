638602655cb75b2ba10234be1c6ef18e80c5f668
Sélénium <francois.milleniumstudio@gmail.com>
2014-01-15 17:56:16 +0100

    Update creature_equip_template size for 5.4.0
    thx @Izidor
    
    Signed-off-by: Sélénium <francois.milleniumstudio@gmail.com>

---
+++ sql/updates/world/2014/01/2014_06_15_world_creature_equip_template.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `creature_equip_template` CHANGE `itemEntry1` `itemEntry1` INT(8) UNSIGNED DEFAULT '0' NOT NULL, CHANGE `itemEntry2` `itemEntry2` INT(8) UNSIGNED DEFAULT '0' NOT NULL, CHANGE `itemEntry3` `itemEntry3` INT(8) UNSIGNED DEFAULT '0' NOT NULL;
