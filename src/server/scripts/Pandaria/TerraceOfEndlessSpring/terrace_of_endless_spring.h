5439846e3b0fb8b81edf6d8b4ff26f564842dfd5
Morgoporc <j.fraisse45@laposte.net>
2014-02-27 16:42:44 +0100

    Add pre-scripts for Terrace of Endless Spring

---
+++ src/server/scripts/Pandaria/TerraceOfEndlessSpring/terrace_of_endless_spring.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2008-20XX JadeCore <http://www.pandashan.com>
+ * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2006-2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TERRACE_OF_ENDLESS_SPRING_H_
+#define TERRACE_OF_ENDLESS_SPRING_H_
+
+#include "SpellScript.h"
+#include "Map.h"
+#include "Creature.h"
+#include "CreatureAIImpl.h"
+
+enum eData
+{
+	DATA_PROTECTORS,
+	DATA_TSULONG,
+	DATA_LEI_SHI,
+	DATA_SHA_OF_FEAR,
+	DATA_MAX_BOSS_DATA
+};
+
+enum eSpells
+{
+};
+
+enum eActions
+{
+};
+
+enum eCreatures
+{
+};
+
+enum eGameObjects
+{
+};
+
+enum ePhases
+{
+};
+
+enum eWeapons
+{
+};
+
+enum eEvents
+{
+};
+
+enum eTypes
+{
+};
+
+#endif // TERRACE_OF_ENDLESS_SPRING_H_
