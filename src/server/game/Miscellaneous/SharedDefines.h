085dc06e66f5e1c3e847e95a55441e827ade2a0e
Izidor <millenium_woweur@hotmail.fr>
2014-03-11 15:58:55 +0100

    Add missing new spell target in 5.4

--- src/server/game/Miscellaneous/SharedDefines.h
+++ src/server/game/Miscellaneous/SharedDefines.h
@@ -1803,6 +1803,7 @@ enum Targets
     TARGET_UNK_140                     = 140,
     TARGET_UNK_141                     = 141,
     TARGET_UNK_142                     = 142,
+    TARGET_UNK_143                     = 143,
     TOTAL_SPELL_TARGETS
 };
 
