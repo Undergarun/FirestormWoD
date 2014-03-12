c9e9f35d0bafc53c5b95de90bd46db6cb8f7037a
Morgoporc <j.fraisse45@laposte.net>
2014-03-12 19:46:27 +0100

    Try fix crash

--- src/server/scripts/EasternKingdoms/ScarletEnclave/chapter1.cpp
+++ src/server/scripts/EasternKingdoms/ScarletEnclave/chapter1.cpp
@@ -168,11 +168,14 @@ public:
             me->RemoveAurasDueToSpell(SPELL_SOUL_PRISON_CHAIN_SELF);
             me->RemoveAurasDueToSpell(SPELL_SOUL_PRISON_CHAIN);
 
-            float z;
-            anchor->GetContactPoint(me, anchorX, anchorY, z, 1.0f);
+            if (anchor)
+            {
+                float z;
+                anchor->GetContactPoint(me, anchorX, anchorY, z, 1.0f);
+            }
 
-            playerGUID = target->GetGUID();
-            DoScriptText(say_event_start[rand()%8], me, target);
+            playerGUID = target ? target->GetGUID() : 0;
+            DoScriptText(say_event_start[urand(0, 7)], me, target);
         }
 
         void UpdateAI(const uint32 diff)
