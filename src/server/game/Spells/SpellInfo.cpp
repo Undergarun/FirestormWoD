cdbb266339a961074b123bfcb7b4899b8c487460
Morgoporc <j.fraisse45@laposte.net>
2014-04-04 20:09:02 +0200

    Fix Sprint cannot remove Stealth
    Signed-off-by: Morgoporc <j.fraisse45@laposte.net>

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -3496,6 +3496,7 @@ bool SpellInfo::IsCustomCharged(SpellInfo const* procSpell) const
                 procSpell->Id == 115192 ||  // Subterfuge
                 procSpell->Id == 114842 ||  // Shadow Walk
                 procSpell->Id == 5171 ||    // Slice and Dice
+                procSpell->Id == 2983 ||    // Sprint
                 procSpell->Id == 98440 ||   // Relentless Strikes
                 procSpell->Id == 12323)     // Piercing Howl
                 return true;
