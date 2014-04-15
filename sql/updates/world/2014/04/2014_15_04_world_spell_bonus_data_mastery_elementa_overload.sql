95cc0d132f1c97212a531cfec86f7cad34feb3ec
Morgoporc <j.fraisse45@laposte.net>
2014-04-15 15:39:44 +0200

    Fix Mastery: Elemental Overload damage
    Signed-off-by: Morgoporc <j.fraisse45@laposte.net>

---
+++ sql/updates/world/2014/04/2014_15_04_world_spell_bonus_data_mastery_elementa_overload.sql
@@ -0,0 +1,3 @@
+UPDATE spell_bonus_data SET direct_bonus = 0.389 WHERE entry = 45297;
+UPDATE spell_bonus_data SET direct_bonus = 1 WHERE entry = 51505;
+UPDATE spell_bonus_data SET direct_bonus = 0.75 WHERE entry = 77451;
