426d3a20ee3441589b12e933eca8047e11878fa5
Izidor <millenium_woweur@hotmail.fr>
2014-04-18 20:14:02 +0200

    Somes fix on Garalon: Ready to be tested live!
    Signed-off-by: Izidor

--- sql/updates/world/2014/04/2014_11_04_world_HOF_Garalon.sql
+++ sql/updates/world/2014/04/2014_11_04_world_HOF_Garalon.sql
@@ -1,8 +1,15 @@
 --  Garalon & Garalon's leg template
 UPDATE creature_template SET minlevel = 93, maxlevel = 93 WHERE entry IN (63053, 62164);
+UPDATE creature_template SET modelid1 = 11686, modelid2 = 0, modelid3 = 0, modelid4 = 0 WHERE entry = 63021;
 UPDATE creature_template SET scriptname = 'boss_garalon' WHERE entry = 62164;
 UPDATE creature_template SET scriptname = 'npc_pheromone_trail' WHERE entry = 63021;
 UPDATE creature_template SET scriptname = 'npc_garalon_leg' WHERE entry = 63053;
+
+INSERT INTO vehicle_template_accessory (entry, accessory_entry, seat_id, minion, description, summontype, summontimer) VALUES (62164, 63053, 0, 1, "Garalon legs", 8, 0);
+INSERT INTO vehicle_template_accessory (entry, accessory_entry, seat_id, minion, description, summontype, summontimer) VALUES (62164, 63053, 1, 1, "Garalon legs", 8, 0);
+INSERT INTO vehicle_template_accessory (entry, accessory_entry, seat_id, minion, description, summontype, summontimer) VALUES (62164, 63053, 2, 1, "Garalon legs", 8, 0);
+INSERT INTO vehicle_template_accessory (entry, accessory_entry, seat_id, minion, description, summontype, summontimer) VALUES (62164, 63053, 3, 1, "Garalon legs", 8, 0);
+INSERT INTO `npc_spellclick_spells`(`npc_entry`,`spell_id`,`cast_flags`,`user_type`) VALUES ( '62164','123432','1','0'); 
 
 -- Spell scripts
 INSERT INTO spell_script_names VALUES
