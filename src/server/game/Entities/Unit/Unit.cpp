91fd9ebb1a5708fcf0cb87909a689b227acb40ab
Morgoporc <j.fraisse45@laposte.net>
2014-05-01 11:24:55 +0200

    Fix Riposte for Warriors
    Signed-off-by: Morgoporc <j.fraisse45@laposte.net>

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -10357,7 +10357,8 @@ bool Unit::HandleProcTriggerSpell(Unit* victim, uint32 damage, AuraEffectPtr tri
 
             break;
         }
-        case 145676:// Riposte
+        case 145672:// Riposte (Warrior)
+        case 145676:// Riposte (Death Knight)
         {
             if (!(procEx & PROC_EX_DODGE) && !(procEx & PROC_EX_PARRY))
                 return false;
