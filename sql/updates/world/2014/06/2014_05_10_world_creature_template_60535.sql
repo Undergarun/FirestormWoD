27868c1f2682866c304cced1c8abf6c665c9dce1
Jeyel <contact.jl@voila.fr>
2014-06-10 10:36:42 +0200

    Fix some quests for faction The August Celestials (1341) + Replace some missing scriptnames
    Signed-off-by: Jeyel <contact.jl@voila.fr>

---
+++ sql/updates/world/2014/06/2014_05_10_world_creature_template_60535.sql
@@ -0,0 +1,12 @@
+#60535/NPC - Julia Bates
+
+	UPDATE `creature_template` SET 
+	`npcflag` = 1, 
+	`mindmg` = 9838, 
+	`maxdmg` = 14331, 
+	`attackpower` = 42296, 
+	`baseattacktime` = 2000, 
+	`rangeattacktime` = 2000, 
+	`ScriptName` = 'mob_julia_bates' 
+	WHERE `entry` = 60535;
+	
---
+++ sql/updates/world/2014/06/2014_05_10_world_creature_template_64008.sql
@@ -0,0 +1,4 @@
+#64008/NPC - Muckscale Slayer
+
+	UPDATE `creature_template` SET `ScriptName` = 'mob_muckscale_shaman' WHERE `entry` = 64008;
+	
