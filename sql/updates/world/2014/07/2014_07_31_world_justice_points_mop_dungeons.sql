f58141da650d81f1dcacd1fb9713ca0abb72c67b
MiWo <mike.werewolf@gmail.com>
2014-07-31 17:36:25 +0200

    Justice points for killing bosses in MOP's dungeons.

---
+++ sql/updates/world/2014/07/2014_07_31_world_justice_points_mop_dungeons.sql
@@ -0,0 +1,21 @@
+-- Justice points for dungeons
+INSERT INTO creature_loot_currency (creature_id, CurrencyId1, CurrencyCount1) VALUES
+-- Temple of the Jade Serpent
+(56439, 395, 8000),
+(56448, 395, 8000),
+(56732, 395, 8000),
+(56843, 395, 8000),
+-- Mogu'shan palace
+(61243, 395, 8000),
+(61398, 395, 8000),
+(61442, 395, 8000),
+-- Shado-Pan Monastery
+(56541, 395, 8000),
+(56747, 395, 8000),
+(56719, 395, 8000),
+(56884, 395, 8000),
+-- Gate of the setting sun
+(56877, 395, 8000),
+(56906, 395, 8000),
+(56589, 395, 8000),
+(56636, 395, 8000);
