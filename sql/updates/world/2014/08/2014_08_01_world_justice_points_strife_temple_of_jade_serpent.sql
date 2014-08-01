e76d92ce927ebd7de9d36ae22ad78e14e4032b3d
MiWo <mike.werewolf@gmail.com>
2014-08-01 18:40:37 +0200

    Temple of the Jade Serpent : Justice points + templates

---
+++ sql/updates/world/2014/08/2014_08_01_world_justice_points_strife_temple_of_jade_serpent.sql
@@ -0,0 +1,4 @@
+-- Justice points for Lorewalker Stonestep (main boss, but loots via other mobs) - Temple of the Jade Serpent
+INSERT INTO creature_loot_currency (creature_id, CurrencyId1, CurrencyCount1) VALUES
+(59051, 395, 8000),
+(590512, 395, 10000);
---
+++ sql/updates/world/2014/08/2014_08_01_world_justice_points_yulon_temple_of_jade_serpent.sql
@@ -0,0 +1,4 @@
+-- Justice points for Liu Flameheart (main boss, but doesn't die so doesn't loot) - Temple of the Jade Serpent
+INSERT INTO creature_loot_currency (creature_id, CurrencyId1, CurrencyCount1) VALUES
+(56762, 395, 8000),
+(567622, 395, 10000);
