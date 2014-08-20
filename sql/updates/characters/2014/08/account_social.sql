c9efac39942b5d40dc6515562cba1e65590c366f
Selenium <francois.milleniumstudio@gmail.com>
2014-08-20 18:01:05 +0200

    rename/change some SQL, add basehp5 in creature_classlevelstat

--- sql/updates/characters/2014/08/account_social.sql
+++ sql/updates/characters/2014/08/account_social.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `character_social` CHANGE `guid` `account_id` INT(10) UNSIGNED DEFAULT '0' NOT NULL COMMENT 'Account Identifier';
-RENAME TABLE `character_social` TO `540_characters`.`account_social`;
+RENAME TABLE `character_social` TO `account_social`;
---
+++ sql/updates/world/2014/08/2014_08_20_world_creature_classlevelstat.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `creature_classlevelstats` ADD `basehp5` MEDIUMINT(8)  UNSIGNED  NOT NULL  DEFAULT '1'  AFTER `basehp4`;
