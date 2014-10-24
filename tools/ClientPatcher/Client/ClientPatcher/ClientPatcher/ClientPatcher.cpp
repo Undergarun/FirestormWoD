c634030b197ede175c1194764fd8ca3571a07b9a
Izidor <millenium_woweur@hotmail.fr>
2014-10-24 19:40:33 +0200

    ClientPatcher/Socket: Patch socket check on JamDispatch (enable packet like SMSG_ATTACK_STOP, SMSG_DUEL_REQUESTS & somes others)

--- tools/ClientPatcher/Client/ClientPatcher/ClientPatcher/ClientPatcher.cpp
+++ tools/ClientPatcher/Client/ClientPatcher/ClientPatcher/ClientPatcher.cpp
@@ -1,13 +1,19 @@
-#define _CRT_SECURE_NO_WARNINGS
+// ClientPatcher.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+
 #include <stdio.h>
 #include <stdint.h>
-                               
-const uint8_t config_data[]         = "Ashran.wtf";
-const uint8_t builddate_data[]      = "[Ashran]";
-const uint8_t bnet_portal_data[]    = "ashran";
-const uint8_t bnet_agent_data[]     = "noagt.exe";
-const uint8_t bnet_versions_data[]  = "http://%s-cdn.ashran.com/%s/versions";
-const uint8_t bnet_cdns_data[]      = "http://%s-cdn.ashran.com/%s/cdns";
+#include <windows.h>
+
+const uint8_t config_data[] = "Ashran.wtf";
+const uint8_t builddate_data[] = "[Ashran]";
+const uint8_t bnet_portal_data[] = "ashran";
+const uint8_t bnet_agent_data[] = "noagt.exe";
+const uint8_t bnet_versions_data[] = "http://%s-cdn.ashran.com/%s/versions";
+const uint8_t bnet_cdns_data[] = "http://%s-cdn.ashran.com/%s/cdns";
+const uint8_t jam_dispatch_check_data[] = {0xEB, 0x1D}; ///< jmp to bypass socket check
 
 #ifndef _countof
 #define _countof(V) (sizeof(V) / sizeof(V[0]))
@@ -30,7 +36,7 @@ struct SystemItem
 
 enum ClientType
 {
-    CLIENT_TYPE_NONE  = 0000000000,
+    CLIENT_TYPE_NONE = 0000000000,
     CLIENT_TYPE_WIN32 = 0x0000014C,
     CLIENT_TYPE_WIN64 = 0x00008664,
     CLIENT_TYPE_MAC32 = 0xFEEDFACE,
@@ -38,33 +44,39 @@ enum ClientType
 };
 
 /// 19027
-const struct RewriteItem Win32Patchs[] = {
-    { 0x0A374D0, sizeof(config_data),                               config_data                                 },   ///< Change default configuration file
-    { 0x0A5B80C, sizeof(builddate_data),                            builddate_data                              },   ///< Change build date
-    { 0x0AFB0A4, sizeof(bnet_portal_data),                          bnet_portal_data                            },   ///< Change bnet portal
-    { 0x0A6722C, sizeof(bnet_agent_data),                           bnet_agent_data                             },   ///< Change bnet agent
-    { 0x0A36760, sizeof(bnet_versions_data),                        bnet_versions_data                          },   ///< Change bnet versions data
-    { 0x0A3678C, sizeof(bnet_cdns_data),                            bnet_cdns_data                              },   ///< Change bnet cdns data
+const struct RewriteItem Win32Patchs[] = 
+{
+    { 0x0A374D0, sizeof(config_data), config_data },                ///< Change default configuration file
+    { 0x0A5B80C, sizeof(builddate_data), builddate_data },          ///< Change build date
+    { 0x0AFB0A4, sizeof(bnet_portal_data), bnet_portal_data },      ///< Change bnet portal
+    { 0x0A6722C, sizeof(bnet_agent_data), bnet_agent_data },        ///< Change bnet agent
+    { 0x0A36760, sizeof(bnet_versions_data), bnet_versions_data },  ///< Change bnet versions data
+    { 0x0A3678C, sizeof(bnet_cdns_data), bnet_cdns_data },          ///< Change bnet cdns data
+    { 0x0248430, 2, jam_dispatch_check_data },                      ///< Bypass jam dispatch socket check
 };
 
-const struct RewriteItem Mac64Patchs[] = {
-    { 0x0000000, sizeof(config_data),                               config_data                                 },   ///< Change default configuration file
-    { 0x0000000, sizeof(builddate_data),                            builddate_data                              },   ///< Change build date
-    { 0x0000000, sizeof(bnet_portal_data),                          bnet_portal_data                            },   ///< Change bnet portal
-    { 0x0000000, sizeof(bnet_agent_data),                           bnet_agent_data                             },   ///< Change bnet agent
-    { 0x0000000, sizeof(bnet_versions_data),                        bnet_versions_data                          },   ///< Change bnet versions data
-    { 0x0000000, sizeof(bnet_cdns_data),                            bnet_cdns_data                              },   ///< Change bnet cdns data
+const struct RewriteItem Mac64Patchs[] = 
+{
+    { 0x0000000, sizeof(config_data), config_data },   ///< Change default configuration file
+    { 0x0000000, sizeof(builddate_data), builddate_data },   ///< Change build date
+    { 0x0000000, sizeof(bnet_portal_data), bnet_portal_data },   ///< Change bnet portal
+    { 0x0000000, sizeof(bnet_agent_data), bnet_agent_data },   ///< Change bnet agent
+    { 0x0000000, sizeof(bnet_versions_data), bnet_versions_data },   ///< Change bnet versions data
+    { 0x0000000, sizeof(bnet_cdns_data), bnet_cdns_data },   ///< Change bnet cdns data
 };
 
 const struct SystemItem gSystems[] = {
-    {"win32",   CLIENT_TYPE_WIN32, _countof(Win32Patchs),  Win32Patchs},
-    {"mac",     CLIENT_TYPE_MAC64, _countof(Mac64Patchs),  Mac64Patchs},
+    { "win32", CLIENT_TYPE_WIN32, _countof(Win32Patchs), Win32Patchs },
+    { "mac", CLIENT_TYPE_MAC64, _countof(Mac64Patchs), Mac64Patchs },
 };
 
 FILE* gFile = 0;
 
 int main(int p_Argc, char* p_Argv[])
 {
+
+    Sleep(10000);
+
     const struct SystemItem     * l_SystemItems;
     const struct RewriteItem    * l_RewriteItem;
 
@@ -109,7 +121,7 @@ int main(int p_Argc, char* p_Argv[])
     }
     else
     {
-        fseek(gFile, 0, SEEK_SET);    
+        fseek(gFile, 0, SEEK_SET);
         fread(&l_ClientType, sizeof(l_ClientType), 1, gFile);
     }
 
