06670f429ab0198d868b815189693cace4c16341
Selenium <francois.milleniumstudio@gmail.com>
2014-10-30 16:57:45 +0100

    Core/Creature: Fix a gap in Basehp and fix somes datas

---
+++ sql/updates/world/2014/10/2014_10_30_world_creature_classlevelstats.sql
@@ -0,0 +1,7 @@
+UPDATE `creature_classlevelstats` SET `basehp4` = '11891' WHERE `level` = '91' AND `class` = '1';
+UPDATE `creature_classlevelstats` SET `basehp4` = '7500' WHERE `level` = '84' AND `class` = '1';
+UPDATE `creature_classlevelstats` SET `basehp4` = '7500', `basemana` = '8726' WHERE `level` = '84' AND `class` = '2';
+UPDATE `creature_classlevelstats` SET `basemana` = '9094' WHERE `level` = '86' AND `class` = '2';
+UPDATE `creature_classlevelstats` SET `basemana` = '9310' WHERE `level` = '87' AND `class` = '2';
+UPDATE `creature_classlevelstats` SET `basemana` = '9470' WHERE `level` = '88' AND `class` = '2';
+UPDATE `creature_classlevelstats` SET `basehp4` = '9195' WHERE `level` = '90' AND `class` = '8';
