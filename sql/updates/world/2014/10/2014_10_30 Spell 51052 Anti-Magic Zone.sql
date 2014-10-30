f177d8b53bd174a30dce42bdd1ec2739728f87d3
Merydwin <merydwin@gmail.com>
2014-10-30 16:36:30 +0100

    Core/Spell 51052 Anti-Magic Zone

---
+++ sql/updates/world/2014/10/2014_10_30 Spell 51052 Anti-Magic Zone.sql
@@ -0,0 +1,8 @@
+/*
+SQLyog Ultimate - MySQL GUI v8.2 
+MySQL - 5.6.17 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `areatrigger_template` (`spell_id`, `eff_index`, `entry`, `type`, `scale_x`, `scale_y`, `flags`, `move_curve_id`, `scale_curve_id`, `morph_curve_id`, `facing_curve_id`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`) values('51052','0','1193','0','6.5','6.5','16384','0','0','0','0','0','0','0','0','0','0','0','0');
