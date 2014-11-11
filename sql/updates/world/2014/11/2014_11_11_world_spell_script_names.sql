6d575e74b695178d673dc9fa74ef7f1ce1e68aa6
pinkmouse <justin.mariette@gmail.com>
2014-11-11 14:26:31 -0600

    Spells/Warrior: Clean up spells

---
+++ sql/updates/world/2014/11/2014_11_11_world_spell_script_names.sql
@@ -0,0 +1,5 @@
+DELETE FROM spell_script_names WHERE spell_id = 112048;
+DELETE FROM spell_script_names WHERE spell_id = 55694;
+DELETE FROM spell_script_names WHERE spell_id = 125831;
+DELETE FROM spell_script_names WHERE spell_id = 56638;
+DELETE FROM spell_script_names WHERE spell_id = 6343;
