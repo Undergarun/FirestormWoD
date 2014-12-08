ce2473f58b5af51b2203bb0cd4343d89d58bdea2
pinkmouse <justin.mariette@gmail.com>
2014-12-08 13:30:37 -0600

    Spells/Paladin : Empowered Hammer of Wrath use now spell_pal_hammer_of_wrath too

--- sql/updates/world/2014/12/2014_12_08_world_spell_script_names.sql
+++ sql/updates/world/2014/12/2014_12_08_world_spell_script_names.sql
@@ -1,2 +1,3 @@
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_dru_ferocious_bite';
 INSERT INTO `spell_script_names` VALUES (22568, 'spell_dru_ferocious_bite');
+INSERT INTO `spell_script_names` VALUES (157496, 'spell_pal_hammer_of_wrath');
