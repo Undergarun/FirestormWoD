8ee073b781e0934c87c728f8f72702e66d7b7641
Selenium <francois.milleniumstudio@gmail.com>
2014-12-11 09:42:35 +0100

    Sql/Typo : add delete and remove multi-queries

--- sql/updates/world/2014/12/2014_12_09_world_access_requirement_challenge.sql
+++ sql/updates/world/2014/12/2014_12_09_world_access_requirement_challenge.sql
@@ -1,32 +1,14 @@
 -- Challenge mode data
-INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `leader_achievement`, `itemlevel_min`, `itemlevel_max`, `quest_failed_text`, `comment`)
-VALUES
-    (1175, 8, 101, 0, 0, 0, 0, 0, 0, 0, 500, 0, NULL, 'Challenge : Bloodmaul Slag Mines');
+DELETE FROM `access_requirement` WHERE `difficulty` = 8; -- Remove all challenge mode
 
+-- Add the WoD entries
 INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `leader_achievement`, `itemlevel_min`, `itemlevel_max`, `quest_failed_text`, `comment`)
 VALUES
-    (1195, 8, 101, 0, 0, 0, 0, 0, 0, 0, 520, 0, NULL, 'Challenge : Iron Docks');
-
-INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `leader_achievement`, `itemlevel_min`, `itemlevel_max`, `quest_failed_text`, `comment`)
-VALUES
-    (1182, 8, 101, 0, 0, 0, 0, 0, 0, 0, 540, 0, NULL, 'Challenge : Auchindoun');
-
-INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `leader_achievement`, `itemlevel_min`, `itemlevel_max`, `quest_failed_text`, `comment`)
-VALUES
-    (1209, 8, 101, 0, 0, 0, 0, 0, 0, 0, 580, 0, NULL, 'Challenge : Skyreach');
-
-INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `leader_achievement`, `itemlevel_min`, `itemlevel_max`, `quest_failed_text`, `comment`)
-VALUES
-    (1208, 8, 101, 0, 0, 0, 0, 0, 0, 0, 580, 0, NULL, 'Challenge : Grimrail Depot');
-
-INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `leader_achievement`, `itemlevel_min`, `itemlevel_max`, `quest_failed_text`, `comment`)
-VALUES
-    (1176, 8, 101, 0, 0, 0, 0, 0, 0, 0, 595, 0, NULL, 'Challenge : Shadowmoon Burial Grounds');
-
-INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `leader_achievement`, `itemlevel_min`, `itemlevel_max`, `quest_failed_text`, `comment`)
-VALUES
-    (1279, 8, 101, 0, 0, 0, 0, 0, 0, 0, 595, 0, NULL, 'Challenge : The Everbloom');
-
-INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `leader_achievement`, `itemlevel_min`, `itemlevel_max`, `quest_failed_text`, `comment`)
-VALUES
+    (1175, 8, 101, 0, 0, 0, 0, 0, 0, 0, 500, 0, NULL, 'Challenge : Bloodmaul Slag Mines'),
+    (1195, 8, 101, 0, 0, 0, 0, 0, 0, 0, 520, 0, NULL, 'Challenge : Iron Docks'),
+    (1182, 8, 101, 0, 0, 0, 0, 0, 0, 0, 540, 0, NULL, 'Challenge : Auchindoun'),
+    (1209, 8, 101, 0, 0, 0, 0, 0, 0, 0, 580, 0, NULL, 'Challenge : Skyreach'),
+    (1208, 8, 101, 0, 0, 0, 0, 0, 0, 0, 580, 0, NULL, 'Challenge : Grimrail Depot'),
+    (1176, 8, 101, 0, 0, 0, 0, 0, 0, 0, 595, 0, NULL, 'Challenge : Shadowmoon Burial Grounds'),
+    (1279, 8, 101, 0, 0, 0, 0, 0, 0, 0, 595, 0, NULL, 'Challenge : The Everbloom'),
     (1358, 8, 101, 0, 0, 0, 0, 0, 0, 0, 595, 0, NULL, 'Challenge : Upper Blackrock Spire');
