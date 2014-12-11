9a06fb0d9e49ea691bfc3b5a2cd9603eadc893c4
Tuxity <kevin.darcel@gmail.com>
2014-12-11 15:02:42 -0600

    Spells/Monk: Fix Tiger Palm

--- sql/updates/world/2014/12/2014_12_11_world_spell_script_names.sql
+++ sql/updates/world/2014/12/2014_12_11_world_spell_script_names.sql
@@ -3,3 +3,5 @@ DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_monk_fists_of_fury'
 INSERT INTO `spell_script_names` VALUES (113656, 'spell_monk_fists_of_fury');
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_monk_jab';
 INSERT INTO `spell_script_names` VALUES (100780, 'spell_monk_jab');
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_monk_tiger_palm';
+INSERT INTO `spell_script_names` VALUES (100787, 'spell_monk_tiger_palm');
