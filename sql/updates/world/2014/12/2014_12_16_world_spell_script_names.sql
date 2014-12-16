b76f0bc32e552307dba0d33631faf58236a58a7a
Tuxity <kevin.darcel@gmail.com>
2014-12-16 17:20:24 -0600

    Fix sql

--- sql/updates/world/2014/12/2014_12_16_world_spell_script_names.sql
+++ sql/updates/world/2014/12/2014_12_16_world_spell_script_names.sql
@@ -6,4 +6,4 @@ INSERT INTO `spell_script_names` VALUES (108853, 'spell_mage_kindling');
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_monk_hurricane_strike';
 INSERT INTO `spell_script_names` VALUES (158221, 'spell_monk_hurricane_strike');
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = '152175' AND `spell_effect` = '158221' AND `type` = '0';
-INSERT INTO `spell_linked_spell` ('152175', '158221', '0', 'Hurricane Strike cast Hurricane Strike (Damage)');
+INSERT INTO `spell_linked_spell` VALUES ('152175', '158221', '0', 'Hurricane Strike cast Hurricane Strike (Damage)');
