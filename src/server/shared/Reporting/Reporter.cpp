70d16c56cb618839dd2bc1769a98d63fdade982b
Bd76 <vincent.l@millenium-studio.fr>
2014-12-31 00:13:18 +0100

    Threading: Rename ScopedMutex to ScopedLock, added some methods and documentation.

--- src/server/shared/Reporting/Reporter.cpp
+++ src/server/shared/Reporting/Reporter.cpp
@@ -17,7 +17,7 @@ namespace MS
         void Reporter::Report(report_type const& p_Query)
         {
             // We lock in order to access the queue.
-            Threading::ScopedMutex l_ScopedMutex(m_Mutex);
+            Threading::ScopedLock l_ScopedMutex(m_Mutex);
 
             m_ReportQueue.emplace(std::move(std::make_unique<report_type>(p_Query)));
         }
@@ -25,7 +25,7 @@ namespace MS
         bool Reporter::HasReports() const
         {
             // We lock in order to access the queue.
-            Threading::ScopedMutex l_ScopedMutex(m_Mutex);
+            Threading::ScopedLock l_ScopedMutex(m_Mutex);
 
             return !m_ReportQueue.empty();
         }
@@ -36,7 +36,7 @@ namespace MS
         Reporter::report_pointer const&& Reporter::Next()
         {
             // We lock in order to access the queue.
-            Threading::ScopedMutex l_ScopedMutex(m_Mutex);
+            Threading::ScopedLock l_ScopedMutex(m_Mutex);
 
             auto const&& l_Report = std::move(m_ReportQueue.front());
             m_ReportQueue.pop();
