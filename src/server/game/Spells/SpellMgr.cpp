384b1f2d7c3d8ba1c05adf8e5483e49052caddf8
pinkmouse <justin.mariette@gmail.com>
2015-01-02 09:10:49 -0600

    Coding style

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -4446,9 +4446,7 @@ void SpellMgr::LoadSpellCustomAttr()
                 spellInfo->Effects[3].BasePoints = 35;
                 break;
             case 165378: // Lethal Shots
-                spellInfo->Effects[0].ApplyAuraName = SPELL_AURA_MOD_CRIT_PCT;
-                break;
-            case 165380: // Sanctified Light
+            case 165380:// Sanctified Light
                 spellInfo->Effects[0].ApplyAuraName = SPELL_AURA_MOD_CRIT_PCT;
                 break;
             case 109306: // Thrill of the Hunt
