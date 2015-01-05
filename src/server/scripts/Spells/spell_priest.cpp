3967a949e70e568d98f6324b80a7a243e4d67f4a
pinkmouse <justin.mariette@gmail.com>
2015-01-05 17:26:52 -0600

    Spells/Priest: Insanity

--- src/server/scripts/Spells/spell_priest.cpp
+++ src/server/scripts/Spells/spell_priest.cpp
@@ -2631,8 +2631,12 @@ public:
     void OnModifyPower(Player* p_Player, Powers p_Power, int32 p_Value)
     {
         if (p_Player->getClass() == CLASS_PRIEST && p_Player->GetSpecializationId(p_Player->GetActiveSpec()) == SPEC_PRIEST_SHADOW && p_Power == POWER_SHADOW_ORB)
-            if (p_Value > 0 && p_Player->HasAura(PRIEST_SPELL_INSANITY_AURA) && roll_chance_i(sSpellMgr->GetSpellInfo(PRIEST_SPELL_INSANITY_AURA)->Effects[EFFECT_0].BasePoints))
+            if (p_Value < 0 && p_Player->HasAura(PRIEST_SPELL_INSANITY_AURA))
+            {
                 p_Player->CastSpell(p_Player, PRIEST_SPELL_INSANITY, true);
+                if (AuraPtr l_Insanity = p_Player->GetAura(PRIEST_SPELL_INSANITY))
+                    l_Insanity->SetDuration(l_Insanity->GetMaxDuration() * (p_Value * -1));
+            }
     }
 };
 
