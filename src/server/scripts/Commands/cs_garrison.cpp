e2d176b84590ce32b12f8f80227974f2f1130e6f
Merydwin <merydwin@gmail.com>
2015-01-06 18:14:41 +0100

    Fix build

--- src/server/scripts/Commands/cs_garrison.cpp
+++ src/server/scripts/Commands/cs_garrison.cpp
@@ -159,7 +159,7 @@ class garrison_commandscript: public CommandScript
             l_TargetPlayer->DeleteGarrison();
 
             if (l_TargetPlayer->GetCurrency(GARRISON_CURRENCY_ID))
-                l_TargetPlayer->ModifyCurrency(GARRISON_CURRENCY_ID, -p_Player->GetCurrency(GARRISON_CURRENCY_ID, false));
+                l_TargetPlayer->ModifyCurrency(GARRISON_CURRENCY_ID, -l_TargetPlayer->GetCurrency(GARRISON_CURRENCY_ID, false));
 
             return true;
         }
