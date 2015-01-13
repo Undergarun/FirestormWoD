4c49564ed09271775bb3a3228444979cb0302004
Merydwin <merydwin@gmail.com>
2015-01-13 17:33:14 +0100

    Core/Garrison disable some part for prod

--- src/server/scripts/Draenor/Garrison/Garrison_H_1.cpp
+++ src/server/scripts/Draenor/Garrison/Garrison_H_1.cpp
@@ -86,6 +86,7 @@ class instance_Garrison_H1 : public InstanceMapScript
             /// Owner can use the garrison cache ?
             virtual bool CanUseGarrisonCache(Player * p_Owner) override
             {
+                return true;
                 if (p_Owner->GetQuestStatus(QUEST_WHAT_WE_GOT) == QUEST_STATUS_REWARDED
                     || p_Owner->HasQuest(QUEST_WHAT_WE_GOT))
                     return true;
