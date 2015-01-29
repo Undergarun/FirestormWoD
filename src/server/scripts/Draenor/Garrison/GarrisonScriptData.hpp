8cc48a4f5cde22a3bd3c22018f77af74c9f6a3e0
Merydwin <merydwin@gmail.com>
2015-01-29 12:17:53 +0100

    Core/Garrison fix alliance storehouse

--- src/server/scripts/Draenor/Garrison/GarrisonScriptData.hpp
+++ src/server/scripts/Draenor/Garrison/GarrisonScriptData.hpp
@@ -52,6 +52,7 @@ namespace MS { namespace Garrison
         QUEST_KEEPING_IT_TOGETHER       = 35176,
         QUEST_SHIP_SALVAGE              = 35166,
         QUEST_PALE_MOONLIGHT            = 35174,
+        LostInTransition                = 37087,
 
         /// Horde
         QUEST_ETABLISH_YOUR_GARRISON_H  = 34378,
