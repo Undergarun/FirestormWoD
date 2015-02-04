1169d24e9d66ba66a1772694d914add2692930f3
pinkmouse <justin.mariette@gmail.com>
2015-02-04 09:49:28 -0600

    Spells/Rogue: Internal Bleeding

--- sql/updates/world/2015/02/2015_02_04_spell_script_names.sql
+++ sql/updates/world/2015/02/2015_02_04_spell_script_names.sql
@@ -1,2 +1,5 @@
 -- Subterfuge script
 INSERT INTO spell_script_names VALUES (115191, "spell_rog_stealth");
+-- Internal Bleending
+DELETE FROM spell_script_names WHERE ScriptName = 'spell_rog_internal_bleeding';
+INSERT INTO spell_script_names VALUE (408, 'spell_rog_internal_bleeding');
