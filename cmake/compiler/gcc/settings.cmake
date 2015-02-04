37de20eddc745e4bbe5419e79bafdd911ab0ffbd
Tuxity <kevin.darcel@gmail.com>
2015-02-04 09:54:03 -0600

    Cmake/GCC: Properly enabled c++14

--- cmake/compiler/gcc/settings.cmake
+++ cmake/compiler/gcc/settings.cmake
@@ -7,6 +7,9 @@ if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS GCC_EXPECTED_VERSION)
   message(FATAL_ERROR "GCC: TrinityCore requires version ${GCC_EXPECTED_VERSION} to build but found ${CMAKE_CXX_COMPILER_VERSION}")
 endif()
 
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
+message(STATUS "GCC: Enabled c++14 support")
+
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
 message(STATUS "GCC: Enabled c++11 support")
 
@@ -22,7 +25,7 @@ endif()
 add_definitions(-DHAVE_SSE2 -D__SSE2__)
 message(STATUS "GCC: SFMT enabled, SSE2 flags forced")
 
-if( WITH_WARNINGS )
+if(WITH_WARNINGS)
   set(WARNING_FLAGS "-W -Wall -Wextra -Winit-self -Winvalid-pch -Wfatal-errors")
   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${WARNING_FLAGS}")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${WARNING_FLAGS} -Woverloaded-virtual")
@@ -32,7 +35,7 @@ else()
   message(STATUS "GCC: All warnings disabled")
 endif()
 
-if( WITH_COREDEBUG )
+if(WITH_COREDEBUG)
   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g3")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g3")
   message(STATUS "GCC: Debug-flags set (-g3)")
