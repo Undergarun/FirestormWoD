5897d2b7f084f4ed6ab77e7ddee372a8544f0444
pinkmouse <justin.mariette@gmail.com>
2015-02-05 10:33:54 -0600

    Spells/Hunter: Spell ID for spell_hun_hunters_mark has change

--- sql/updates/world/2015/02/2015_02_05_spell_script_names.sql
+++ sql/updates/world/2015/02/2015_02_05_spell_script_names.sql
@@ -1,1 +1,3 @@
 INSERT INTO spell_script_names VALUES (82691, "spell_mage_ring_of_frost_immunity");
+DELETE FROM spell_script_names WHERE ScriptName = 'spell_hun_hunters_mark';
+INSERT INTO spell_script_names VALUE (1130, 'spell_hun_hunters_mark');
