78d2c6349bfa88dcca409b48a89f7065bd054f8f
pinkmouse <justin.mariette@gmail.com>
2015-02-05 12:25:04 -0600

    Spells/Hunter: Add Smack to basic attack of pet

--- sql/updates/world/2015/02/2015_02_05_spell_script_names.sql
+++ sql/updates/world/2015/02/2015_02_05_spell_script_names.sql
@@ -1,3 +1,7 @@
 INSERT INTO spell_script_names VALUES (82691, "spell_mage_ring_of_frost_immunity");
 DELETE FROM spell_script_names WHERE ScriptName = 'spell_hun_hunters_mark';
 INSERT INTO spell_script_names VALUE (1130, 'spell_hun_hunters_mark');
+DELETE FROM spell_script_names WHERE ScriptName = 'spell_hun_claw_bite';
+INSERT INTO spell_script_names VALUE (49966, 'spell_hun_claw_bite');
+INSERT INTO spell_script_names VALUE (16827, 'spell_hun_claw_bite');
+INSERT INTO spell_script_names VALUE (17253, 'spell_hun_claw_bite');
