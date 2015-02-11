191c0143d70659777680ed49c615089fc051877a
pinkmouse <justin.mariette@gmail.com>
2015-02-11 17:44:02 -0600

    Spells/Monk: Delete useless SQL spell script

--- sql/updates/world/2015/02/2015_02_11_spell_script_names.sql
+++ sql/updates/world/2015/02/2015_02_11_spell_script_names.sql
@@ -2,3 +2,5 @@ DELETE FROM spell_script_names WHERE ScriptName = 'spell_monk_rushing_jade_wind_
 INSERT INTO spell_script_names VALUE (162530, 'spell_monk_rushing_jade_wind_heal');
 DELETE FROM spell_script_names WHERE ScriptName = 'spell_monk_rushing_jade_wind_damage';
 INSERT INTO spell_script_names VALUE (148187, 'spell_monk_rushing_jade_wind_damage');
+DELETE FROM spell_script_names WHERE ScriptName = 'spell_monk_serpents_zeal';
+DELETE FROM spell_script_names WHERE ScriptName = 'spell_monk_muscle_memory';
