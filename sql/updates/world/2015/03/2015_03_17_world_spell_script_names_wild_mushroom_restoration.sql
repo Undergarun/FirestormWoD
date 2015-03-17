e65e64385e699797cfeadf3f11bfd3c21abafe37
pinkmouse <justin.mariette@gmail.com>
2015-03-17 13:58:05 -0600

    Sql Wild Mushroom

---
+++ sql/updates/world/2015/03/2015_03_17_world_spell_script_names_wild_mushroom_restoration.sql
@@ -0,0 +1,5 @@
+DELETE FROM spell_script_names WHERE ScriptName = 'spell_dru_wild_mushroom_heal';
+INSERT INTO spell_script_names VALUES (81262, 'spell_dru_wild_mushroom_heal');
+
+DELETE FROM spell_script_names WHERE ScriptName = 'spell_dru_wild_mushroom_heal_proc';
+INSERT INTO spell_script_names VALUES (81269, 'spell_dru_wild_mushroom_heal_proc');
