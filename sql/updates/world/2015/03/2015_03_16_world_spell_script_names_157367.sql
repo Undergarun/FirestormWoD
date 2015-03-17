586d436016950ae790885a8d037c1a2ba5d5f8bf
Alexandre SALVI <alexandre.salvi@live.fr>
2015-03-17 16:43:56 +0100

    Sql/Script: fix mistake
    Signed-off-by: Alexandre SALVI <alexandre.salvi@live.fr>

--- sql/updates/world/2015/03/2015_03_16_world_spell_script_names_157367.sql
+++ sql/updates/world/2015/03/2015_03_16_world_spell_script_names_157367.sql
@@ -1,2 +1,2 @@
-INSERT INTO spell_script_names VALUES (157367, 'spell_dk_improved_death_grip');
+DELETE FROM spell_script_names WHERE ScriptName = 'spell_dk_improved_death_grip';
 INSERT INTO spell_script_names VALUES (49576, 'spell_dk_improved_death_grip');
