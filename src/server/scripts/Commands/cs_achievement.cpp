c72df941e7358b54001a7455e995f7d410e91a29
Izidor <millenium_woweur@hotmail.fr>
2015-03-24 20:20:05 +0100

    Database/Achievement: Add new achievement reward for WoD. Just missing spell reward, but we don't handle it ATM.

--- src/server/scripts/Commands/cs_achievement.cpp
+++ src/server/scripts/Commands/cs_achievement.cpp
@@ -34,8 +34,9 @@ public:
     {
         static ChatCommand achievementCommandTable[] =
         {
-            { "add",            SEC_ADMINISTRATOR,  false,  &HandleAchievementAddCommand,      "", NULL },
-            { NULL,             0,                  false,  NULL,                              "", NULL }
+            { "add",            SEC_ADMINISTRATOR,  false,  &HandleAchievementAddCommand,            "", NULL },
+            { "generatereward", SEC_ADMINISTRATOR,  false,  &HandleAchievementGenerateRewardCommand, "", NULL },
+            { NULL,             0,                  false,  NULL,                                    "", NULL }
         };
         static ChatCommand commandTable[] =
         {
@@ -71,6 +72,51 @@ public:
             target->CompletedAchievement(achievementEntry);
 
         return true;
+    }
+
+    static bool HandleAchievementGenerateRewardCommand(ChatHandler* p_ChatHandler, char const* p_Args)
+    {
+        FILE* l_File = fopen("AchievementReward.sql", "w+");
+        if (l_File == nullptr)
+            return false;
+
+        for (uint32 l_I = 0; l_I < sAchievementStore.GetNumRows(); l_I++)
+        {
+            auto l_AchievementEntry = sAchievementStore.LookupEntry(l_I);
+            if (l_AchievementEntry == nullptr)
+                continue;
+
+            std::string l_RewardText = l_AchievementEntry->RewardLang;
+            if (l_RewardText.empty())
+                continue;
+
+
+            /// - Handle title
+            std::string l_Match = "Title Reward: ";
+            if (l_RewardText.find(l_Match) != std::string::npos)
+            {
+                std::string l_TitleNameToMatch = l_RewardText.substr(l_Match.size());
+
+                for (uint32 l_TitleIdx = 0; l_TitleIdx < sCharTitlesStore.GetNumRows(); l_TitleIdx++)
+                {
+                    auto l_TitleEntry = sCharTitlesStore.LookupEntry(l_TitleIdx);
+                    if (l_TitleEntry == nullptr)
+                        continue;
+
+                    std::string l_TitleName = l_TitleEntry->NameLang;
+                    if (l_TitleName.find(l_TitleNameToMatch) != std::string::npos)
+                    {
+                        std::ostringstream l_Query;
+                        l_Query << "INSERT INTO achievement_reward (entry, title_A, title_H) VALUES (" << l_AchievementEntry->ID << ", " << l_TitleEntry->ID << ", " << l_TitleEntry->ID << ");\r\n";
+                        fputs(l_Query.str().c_str(), l_File);
+                        break;
+                    }
+                }
+            }
+        }
+
+        fflush(l_File);
+        fclose(l_File);
     }
 };
 
