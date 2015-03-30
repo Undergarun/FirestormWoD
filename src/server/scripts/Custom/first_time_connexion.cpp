9c10e567d7272f5b658e51750d44572d2c5a15fd
Morgoporc <j.fraisse45@laposte.net>
2015-03-30 10:21:55 +0200

    Revert "Core/Spells: Fix SPELL_AURA_MOD_RATING_PCT is not just visual anymore"
    This reverts commit eaaf129acb4628256ae78bd4dee8d2eee7aad970.

--- src/server/scripts/Custom/first_time_connexion.cpp
+++ src/server/scripts/Custom/first_time_connexion.cpp
@@ -4,14 +4,16 @@
 
 class ReportingOnFirstTimeConnexion : public PlayerScript
 {
-    public:
-        ReportingOnFirstTimeConnexion() : PlayerScript("ReportingOnFirstTimeConnexion") {}
+public:
+    ReportingOnFirstTimeConnexion() : PlayerScript("ReportingOnFirstTimeConnexion") {}
 
-        void OnLogin(Player* p_Player)
+    void OnLogin(Player* p_Player)
+    {
+        if (p_Player && p_Player->GetTotalPlayedTime() == 0)
         {
-            if (p_Player && p_Player->GetTotalPlayedTime() == 0)
-                sReporter->Report(MS::Reporting::MakeReport<MS::Reporting::Opcodes::FirstTimeConnexion>::Craft(p_Player->GetSession()->GetAccountId(), p_Player->getRace(), p_Player->getClass()));
+            sReporter->Report(MS::Reporting::MakeReport<MS::Reporting::Opcodes::FirstTimeConnexion>::Craft(p_Player->GetSession()->GetAccountId(), p_Player->getRace(), p_Player->getClass()));
         }
+    }
 };
 
 void AddSC_first_time_connexion()
