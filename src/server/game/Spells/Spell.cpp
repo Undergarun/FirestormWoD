5e7e2e0d1bcd65b554ea1fcb053814054bfe95da
Izidor <millenium_woweur@hotmail.fr>
2015-04-05 19:04:40 +0200

    Core/Spell: Backport spellcharge fix from Pandashan.

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -5942,7 +5942,7 @@ SpellCastResult Spell::CheckCast(bool strict)
         if (categories && categories->ChargesCategory != 0)
         {
             auto const category = sSpellCategoryStores.LookupEntry(categories->ChargesCategory);
-            if (category && category->MaxCharges != 0 && !player->CanUseCharge(m_spellInfo->Id))
+            if (category && category->MaxCharges != 0 && !player->CanUseCharge(categories->Id))
                 return m_triggeredByAuraSpell ? SPELL_FAILED_DONT_REPORT : SPELL_FAILED_NOT_READY;
         }
     }
