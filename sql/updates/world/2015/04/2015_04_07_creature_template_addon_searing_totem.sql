b55004cfea5f2d7958ae574a7a4d8c92bd87be78
Tuxity <kevin.darcel@gmail.com>
2015-04-07 16:44:24 -0500

    Fix travis build on my sql

--- sql/updates/world/2015/04/2015_04_07_creature_template_addon_searing_totem.sql
+++ sql/updates/world/2015/04/2015_04_07_creature_template_addon_searing_totem.sql
@@ -1,1 +1,2 @@
-UPDATE `ashran_world`.`creature_template_addon` SET `auras` = '' WHERE `entry` = 2523;
+DELETE FROM `creature_template_addon` WHERE `entry` = 2523;
+INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES (2523,0,0,0,2,0,'');
