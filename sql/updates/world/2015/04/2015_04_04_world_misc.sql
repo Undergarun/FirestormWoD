18327fa300d1e0d814714bcbaa99e6844183585c
François B <francois@millenium-studio.fr>
2015-04-08 12:27:41 +0200

    Sql/Travis: Add delete statement 2

--- sql/updates/world/2015/04/2015_04_04_world_misc.sql
+++ sql/updates/world/2015/04/2015_04_04_world_misc.sql
@@ -17,10 +17,14 @@ UPDATE `creature_template` SET `gossip_menu_id` = 0 WHERE `entry` in (2409, 931)
 update creature_template set `gossip_menu_id` = 0 where npcflag=8193 and gossip_menu_id !='0';
 
 -- fix Marked for Death
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_rog_marked_for_death';
 INSERT INTO `spell_script_names` VALUES ('140149', 'spell_rog_marked_for_death'); -- visual
+
+DELETE FROM `spell_linked_spell` WHERE spell_trigger = 137619;
 INSERT INTO `spell_linked_spell` VALUES (137619, 140149, 2, 'Marked for Death (check caster)');
 
 -- fix Nightfall proc from Corruption
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_warl_nightfall';
 INSERT INTO `spell_script_names` VALUES (146739, 'spell_warl_nightfall');
 
 -- missing entry key, speedup table.
