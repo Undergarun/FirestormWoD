9df7e8780ba9a589a87a2119bfb899dbd592eb6e
Jeyel <contact.jl@voila.fr>
2015-04-10 16:45:20 +0200

    SQL/Rukhmar - Update oblivions
    Signed-off-by: Jeyel <contact.jl@voila.fr>

--- sql/updates/world/2015/04/2015_04_08_world_creature_template.sql
+++ sql/updates/world/2015/04/2015_04_08_world_creature_template.sql
@@ -14,17 +14,26 @@
 	INSERT INTO spell_script_names VALUE (167629, "spell_rukhmar_blaze_of_glory");
 	INSERT INTO spell_script_names VALUE (167647, "spell_rukhmar_loose_quills");
 
-
-
-
-
-
-/*
-DEV LINE : 
-
-	SCRIPT PILE OF ASH
-	SCRIPT DEPLETED PHOENIX
-	SCRIPT ENERGIZED PHOENIX
-
-SELECT * FROM waypoint_data WHERE id = 83746;
-*/
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','23','131.522','2432.68','169.563','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','22','78.886','2277.61','256.955','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','21','27.9047','2108.97','204.323','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','20','-23.4536','2064.19','187.723','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','19','-148.058','2066.57','184.298','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','18','-248.557','2115.8','163.153','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','17','-328.726','2153.99','151.22','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','16','-431.643','2148.71','151.13','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','15','-491.414','2052.99','152.572','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','14','-500.702','1932.53','159.579','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','13','-420.802','1855.36','155.66','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','12','-313.779','1852.22','151.363','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','11','-228.946','1901.49','155.93','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','10','-234.986','2008.37','161.793','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','9','-249.771','2115.97','163.267','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','8','-265.634','2227.69','155.873','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','7','-271.158','2333.12','139.949','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','6','-247.81','2439.22','110.047','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','5','-193.294','2483.9','91.3236','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','4','-131.968','2531.62','133.638','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','3','-9.79054','2587.94','134.577','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','2','47.6894','2560.34','135.523','0','0','0','0','100','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_flag`, `action`, `action_chance`, `wpguid`) values('83746','1','105.782','2520.86','133.07','0','0','0','0','100','0');
