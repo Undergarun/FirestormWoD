f11e2a7416c70a58b8a844191f5140f2b4dc728a
Selenium <francois@millenium-studio.fr>
2015-04-14 16:51:25 +0200

    Travis : Try another way for validate the new field

--- sql/base/world_database.sql
+++ sql/base/world_database.sql
@@ -2701,7 +2701,6 @@ CREATE TABLE `npc_vendor` (
   `incrtime` int(10) unsigned NOT NULL DEFAULT '0',
   `ExtendedCost` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `type` tinyint(3) unsigned NOT NULL DEFAULT '1',
-  `PlayerConditionID` int(10) unsigned DEFAULT '0',
   PRIMARY KEY (`entry`,`item`,`ExtendedCost`,`type`),
   KEY `slot` (`slot`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Npc System';
