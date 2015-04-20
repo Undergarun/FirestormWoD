fb40f58e2fdfb34e76a735993080821618a3fa50
Tuxity <kevin.darcel@gmail.com>
2015-04-20 15:29:56 -0500

    Spells/Shaman: Fix Earthquake

---
+++ sql/updates/world/2015/04/2015_04_20_world_areatrigger_template_earthquake.sql
@@ -0,0 +1,3 @@
+DELETE FROM `areatrigger_template` WHERE `spell_id` = 61882 AND `entry` = 8382;
+INSERT INTO `areatrigger_template` (`spell_id`, `eff_index`, `entry`, `type`, `scale_x`, `scale_y`, `flags`, `move_curve_id`, `scale_curve_id`, `morph_curve_id`, `facing_curve_id`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `ScriptName`, `creature_visual`) VALUES
+(61882,2,8382,0,1,1,8192,0,0,0,0,8,8,4,4,1,1,0,0,NULL,0);
--- src/server/scripts/Spells/spell_shaman.cpp
+++ src/server/scripts/Spells/spell_shaman.cpp
@@ -45,9 +45,6 @@ enum ShamanSpells
     SPELL_SHA_HEALING_RAIN                      = 142923,
     SPELL_SHA_HEALING_RAIN_TICK                 = 73921,
     SPELL_SHA_HEALING_RAIN_AURA                 = 73920,
-    SPELL_SHA_EARTHQUAKE                        = 61882,
-    SPELL_SHA_EARTHQUAKE_TICK                   = 77478,
-    SPELL_SHA_EARTHQUAKE_KNOCKING_DOWN          = 77505,
     SPELL_SHA_ELEMENTAL_BLAST                   = 117014,
     SPELL_SHA_ELEMENTAL_BLAST_NATURE_VISUAL     = 118517,
     SPELL_SHA_ELEMENTAL_BLAST_FROST_VISUAL      = 118515,
@@ -1234,45 +1231,8 @@ class spell_sha_elemental_blast: public SpellScriptLoader
         }
 };
 
-/// Earthquake : Ticks - 77478
-class spell_sha_earthquake_tick: public SpellScriptLoader
-{
-    public:
-        spell_sha_earthquake_tick() : SpellScriptLoader("spell_sha_earthquake_tick") { }
-
-        class spell_sha_earthquake_tick_SpellScript : public SpellScript
-        {
-            PrepareSpellScript(spell_sha_earthquake_tick_SpellScript);
-
-            bool Validate(SpellInfo const* /*spell*/)
-            {
-                if (!sSpellMgr->GetSpellInfo(SPELL_SHA_EARTHQUAKE_TICK))
-                    return false;
-                return true;
-            }
-
-            void HandleOnHit()
-            {
-                // With a 10% chance of knocking down affected targets
-                if (Unit* caster = GetCaster())
-                    if (Unit* target = GetHitUnit())
-                        if (roll_chance_i(GetSpellInfo()->Effects[EFFECT_1].BasePoints))
-                            caster->CastSpell(target, SPELL_SHA_EARTHQUAKE_KNOCKING_DOWN, true);
-            }
-
-            void Register()
-            {
-                OnHit += SpellHitFn(spell_sha_earthquake_tick_SpellScript::HandleOnHit);
-            }
-        };
-
-        SpellScript* GetSpellScript() const
-        {
-            return new spell_sha_earthquake_tick_SpellScript();
-        }
-};
-
 /// Earthquake - 61882
+/// last update : 6.1.2 19802
 class spell_sha_earthquake: public SpellScriptLoader
 {
     public:
@@ -1282,32 +1242,39 @@ class spell_sha_earthquake: public SpellScriptLoader
         {
             PrepareAuraScript(spell_sha_earthquake_AuraScript);
 
-            void OnApply(constAuraEffectPtr aurEff, AuraEffectHandleModes /*mode*/)
+            enum eSpells
             {
-                 m_PctBonus = 1.0f;
-
-                if (AuraPtr l_Aura = GetCaster()->GetAura(SPELL_SHA_IMPROVED_CHAIN_LIGHTNING))
-                {
-                    m_PctBonus = l_Aura->GetEffect(EFFECT_0)->GetAmount() / 100.0f;
-                    l_Aura->Remove();
-                }
-            }
+                Earthquake     = 61882,
+                EarthquakeTick = 77478
+            };
 
             void OnTick(constAuraEffectPtr aurEff)
             {
-                int32 l_bp0 = GetSpellInfo()->Effects[EFFECT_0].CalcValue(GetCaster()) * m_PctBonus;
+                Unit* l_Caster = GetCaster();
+                if (!l_Caster)
+                    return;
 
-                if (Unit* caster = GetCaster())
-                    if (DynamicObject* dynObj = caster->GetDynObject(SPELL_SHA_EARTHQUAKE))
-                        caster->CastCustomSpell(dynObj->GetPositionX(), dynObj->GetPositionY(), dynObj->GetPositionZ(), SPELL_SHA_EARTHQUAKE_TICK, &l_bp0, nullptr, nullptr, true);
+                AreaTrigger* l_AreaTrigger = l_Caster->GetAreaTrigger(eSpells::Earthquake);
+                if (!l_AreaTrigger)
+                    return;
+
+                /// dealing ${$SPN * 0.11 * 10 * (1 + $170374m3 / 100)} Physical damage over $d
+                int32 l_Bp0 = l_Caster->SpellBaseDamageBonusDone(SPELL_SCHOOL_MASK_SPELL) * 0.11 * 10;
+
+                if (AuraPtr l_ChainLightning = l_Caster->GetAura(SPELL_SHA_IMPROVED_CHAIN_LIGHTNING))
+                {
+                    l_Bp0 += CalculatePct(l_Bp0, l_ChainLightning->GetEffect(EFFECT_0)->GetAmount());
+                    l_ChainLightning->Remove();
+                }
+
+                l_Bp0 /= GetSpellInfo()->GetDuration() / IN_MILLISECONDS;
+
+                l_Caster->CastCustomSpell(l_AreaTrigger->GetPositionX(), l_AreaTrigger->GetPositionY(), l_AreaTrigger->GetPositionZ(), eSpells::EarthquakeTick, &l_Bp0, nullptr, nullptr, true);
             }
-
-            float m_PctBonus;
 
             void Register()
             {
                 OnEffectPeriodic += AuraEffectPeriodicFn(spell_sha_earthquake_AuraScript::OnTick, EFFECT_1, SPELL_AURA_PERIODIC_DUMMY);
-                OnEffectApply += AuraEffectApplyFn(spell_sha_earthquake_AuraScript::OnApply, EFFECT_1, SPELL_AURA_PERIODIC_DUMMY, AURA_EFFECT_HANDLE_REAL);
             }
         };
 
@@ -1315,6 +1282,54 @@ class spell_sha_earthquake: public SpellScriptLoader
         {
             return new spell_sha_earthquake_AuraScript();
         }
+};
+
+/// Earthquake : Ticks - 77478
+/// last update : 6.1.2 19802
+class spell_sha_earthquake_tick: public SpellScriptLoader
+{
+public:
+    spell_sha_earthquake_tick() : SpellScriptLoader("spell_sha_earthquake_tick") { }
+
+    class spell_sha_earthquake_tick_SpellScript : public SpellScript
+    {
+        PrepareSpellScript(spell_sha_earthquake_tick_SpellScript);
+
+        enum eSpells
+        {
+            EarthquakeTick      = 77478,
+            EarthquakeKnockDown = 77505
+        };
+
+        bool Validate(SpellInfo const* /*spell*/)
+        {
+            if (!sSpellMgr->GetSpellInfo(eSpells::EarthquakeTick))
+                return false;
+            return true;
+        }
+
+        void HandleOnHit()
+        {
+            Unit* l_Caster = GetCaster();
+            Unit* l_Target = GetHitUnit();
+            if (!l_Caster || !l_Target)
+                return;
+
+            /// 10% chance of knocking down affected targets
+            if (roll_chance_i(GetSpellInfo()->Effects[EFFECT_1].BasePoints))
+                l_Caster->CastSpell(l_Target, eSpells::EarthquakeKnockDown, true);
+        }
+
+        void Register()
+        {
+            OnHit += SpellHitFn(spell_sha_earthquake_tick_SpellScript::HandleOnHit);
+        }
+    };
+
+    SpellScript* GetSpellScript() const
+    {
+        return new spell_sha_earthquake_tick_SpellScript();
+    }
 };
 
 /// Healing Rain - 73920
