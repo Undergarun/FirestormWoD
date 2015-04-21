5c123d5c0697ed4ef7e4a0b95773eabb7e547d00
pinkmouse <justin.mariette@gmail.com>
2015-04-21 15:34:18 -0500

    Core/Spells: Chimera shot 20% nerf on pvp

---
+++ sql/updates/world/2015/04/2015_04_21_world_spell_script_names_chimera_shot.sql
@@ -0,0 +1,3 @@
+DELETE FROM spell_script_names WHERE ScriptName = 'spell_hun_chimaera_shot_damage';
+INSERT INTO spell_script_names VALUES (171454, 'spell_hun_chimaera_shot_damage');
+INSERT INTO spell_script_names VALUES (171457, 'spell_hun_chimaera_shot_damage');
