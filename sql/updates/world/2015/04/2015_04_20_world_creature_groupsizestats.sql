1f2b0e87f5ca47176bd5cf96942ea942220887ff
Izidor <millenium_woweur@hotmail.fr>
2015-04-21 11:12:32 +0200

    Database/CreatureGroupSizeStates: Add missing table structure.

--- sql/updates/world/2015/04/2015_04_20_world_creature_groupsizestats.sql
+++ sql/updates/world/2015/04/2015_04_20_world_creature_groupsizestats.sql
@@ -1,28 +1,13 @@
 /*
 SQLyog Ultimate v9.02 
-MySQL - 5.5.42-37.1 
+MySQL - 5.6.19-log 
 *********************************************************************
 */
 /*!40101 SET NAMES utf8 */;
 
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','10','31393993');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','11','34596975');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','12','37928007');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','13','41172856');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','14','44459994');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','15','47806270');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','16','51065209');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','17','54436062');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','18','57648012');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','19','60893322');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','20','64198285');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','21','67849874');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','22','70810880');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','23','74128464');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','24','78697130');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','25','81255488');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','26','83924296');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','27','87271456');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','28','91722098');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','29','95560641');
-insert into `creature_groupsizestats` (`entry`, `difficulty`, `groupSize`, `health`) values('78714','14','30','97147609');
+create table `creature_groupsizestats` (
+	`entry` int (11),
+	`difficulty` int (11),
+	`groupSize` int (11),
+	`health` int (11)
+); 
