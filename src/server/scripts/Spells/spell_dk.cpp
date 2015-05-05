50131647f73f7ca5a92f6130241f86b765206372
pinkmouse <justin.mariette@gmail.com>
2015-05-05 09:44:13 -0500

    Spells/DK: Fix target of Blood boil, don't need to have a Selected target

--- src/server/scripts/Spells/spell_dk.cpp
+++ src/server/scripts/Spells/spell_dk.cpp
@@ -1488,13 +1488,10 @@ class spell_dk_blood_boil: public SpellScriptLoader
 
                 Unit* l_Target = l_Player->GetSelectedUnit();
 
-                if (l_Target == nullptr)
+                if (l_Target != nullptr && !l_Player->IsValidAttackTarget(l_Target))
                     return SPELL_FAILED_NO_VALID_TARGETS;
 
-                if (!l_Player->IsValidAttackTarget(l_Target))
-                    return SPELL_FAILED_NO_VALID_TARGETS;
-
-                if (l_Player->GetDistance(l_Target) > GetSpellInfo()->Effects[EFFECT_0].RadiusEntry->radiusHostile)
+                if (l_Target != nullptr && l_Player->GetDistance(l_Target) > GetSpellInfo()->Effects[EFFECT_0].RadiusEntry->radiusHostile)
                     return SPELL_FAILED_OUT_OF_RANGE;
 
                 return SPELL_CAST_OK;
