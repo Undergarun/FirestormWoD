30dbac23a966b85aab33ce808e4d9d0036d41d2b
twink <nikazhazha@mail.ru>
2015-05-17 17:55:13 +0300

    Fix attack speed of all pets.

--- sql/updates/world/2015_05_18_world_spell_fixes_twink.sql
+++ sql/updates/world/2015_05_18_world_spell_fixes_twink.sql
@@ -1,3 +1,6 @@
 -- fix Sanctified Wrath
 DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_pal_sanctified_wrath';
 INSERT INTO `spell_script_names` VALUES (31884, 'spell_pal_sanctified_wrath');
+
+-- fix attack speed of all pets
+UPDATE `pet_stats` SET `attackspeed`='2' WHERE `entry`='1';
