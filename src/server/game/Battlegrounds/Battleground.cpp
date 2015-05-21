9233564f40ec96ff7278fead8951b8e01294cb48
Morgoporc <j.fraisse45@laposte.net>
2015-05-21 10:15:54 +0200

    Core/Groups : Fix warning and remove "fullUpdate" parameter I don't know who did this, but he's a fucking lazier...
    Signed-off-by: Morgoporc <j.fraisse45@laposte.net>

--- src/server/game/Battlegrounds/Battleground.cpp
+++ src/server/game/Battlegrounds/Battleground.cpp
@@ -611,7 +611,7 @@ inline void Battleground::_ProcessJoin(uint32 diff)
                         if (Player* l_Member = _GetPlayerForTeam(GetOtherTeam(l_Player->GetBGTeam()), l_Iterator, "BuildArenaOpponentSpecializations"))
                         {
                             WorldPacket l_Data;
-                            l_Player->GetSession()->BuildPartyMemberStatsChangedPacket(l_Member, &l_Data, GROUP_UPDATE_FULL, true, true);
+                            l_Player->GetSession()->BuildPartyMemberStatsChangedPacket(l_Member, &l_Data, GROUP_UPDATE_FULL, true);
                             l_Player->GetSession()->SendPacket(&l_Data);
                         }
                     }
