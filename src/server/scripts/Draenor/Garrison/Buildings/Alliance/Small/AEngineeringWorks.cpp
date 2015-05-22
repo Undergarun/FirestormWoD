a8b28bffa6a0976f1bd1f98a139c24cda81ea75f
Merydwin <merydwin@gmail.com>
2015-05-22 18:00:58 +0200

    Core/Garrison new alliance building Engineer Works

---
+++ src/server/scripts/Draenor/Garrison/Buildings/Alliance/Small/AEngineeringWorks.cpp
@@ -0,0 +1,84 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  MILLENIUM-STUDIO
+//  Copyright 2014-2015 Millenium-studio SARL
+//  All Rights Reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+#include "AEngineeringWorks.hpp"
+#include "ScriptMgr.h"
+#include "ScriptedCreature.h"
+#include "ScriptedGossip.h"
+#include "GameObjectAI.h"
+#include "Spell.h"
+#include "GarrisonMgr.hpp"
+
+namespace MS { namespace Garrison 
+{
+    //////////////////////////////////////////////////////////////////////////
+    /// 77365 - Zaren                                                     ////
+    //////////////////////////////////////////////////////////////////////////
+    namespace npc_ZarenAIData
+    {
+        InitSequenceFunction FnLevel1 = [](GarrisonNPCAI* p_This, Creature* p_Me)
+        {
+
+        };
+
+        InitSequenceFunction FnLevel2 = [](GarrisonNPCAI* p_This, Creature* p_Me)
+        {
+
+        };
+
+        InitSequenceFunction FnLevel3 = [](GarrisonNPCAI* p_This, Creature* p_Me)
+        {
+
+        };
+
+        char ScriptName[] = "npc_Pozzlow_Garr";
+
+        std::vector<SkillNPC_RecipeEntry> Recipes
+        {
+            { 176732, 31233 },
+            { 173309,     0 },
+            { 173308,     0 },
+            { 173289, 31232 },
+            { 162217, 31233 },
+            { 162214, 31233 },
+            { 162210, 31233 },
+            { 162209, 31233 },
+            { 162208,     0 },
+            { 162207,     0 },
+            { 162205, 31232 },
+            { 162204,     0 },
+            { 162203, 31232 },
+            { 162202, 31232 },
+            { 162199, 31233 }
+        };
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    /// 77831 - Helayn Whent                                              ////
+    //////////////////////////////////////////////////////////////////////////
+    namespace npc_HelaynWhentAIData
+    {
+        InitSequenceFunction FnLevel1 = [](GarrisonNPCAI* p_This, Creature* p_Me)
+        {
+
+        };
+
+        InitSequenceFunction FnLevel2 = [](GarrisonNPCAI* p_This, Creature* p_Me)
+        {
+
+        };
+
+        InitSequenceFunction FnLevel3 = [](GarrisonNPCAI* p_This, Creature* p_Me)
+        {
+
+        };
+
+        char ScriptName[] = "npc_GarbraFizzwonk_Garr";
+    }
+
+}   ///< namespace Garrison
+}   ///< namespace MS
