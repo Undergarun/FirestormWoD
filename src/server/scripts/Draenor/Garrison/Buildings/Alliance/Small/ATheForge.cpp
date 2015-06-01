fbb390bb27c8af4f384405bafd20c02d9c115c67
merydwin <merydwin@gmail.com>
2015-06-01 05:17:36 +0200

    Core/Garrison fix the forge

--- src/server/scripts/Draenor/Garrison/Buildings/Alliance/Small/ATheForge.cpp
+++ src/server/scripts/Draenor/Garrison/Buildings/Alliance/Small/ATheForge.cpp
@@ -19,176 +19,135 @@ namespace MS { namespace Garrison
     //////////////////////////////////////////////////////////////////////////
     /// 77359 - Auria Irondreamer                                         ////
     //////////////////////////////////////////////////////////////////////////
-
-    /// Constructor
-    npc_AuriaIrondreamer::npc_AuriaIrondreamer()
-        : CreatureScript("npc_AuriaIrondreamer_Garr")
+    namespace npc_CostanHighwallAIData
     {
-
-    }
-
-    //////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////
-
-    /// Called when a player opens a gossip dialog with the GameObject.
-    /// @p_Player     : Source player instance
-    /// @p_Creature   : Target GameObject instance
-    bool npc_AuriaIrondreamer::OnGossipHello(Player * p_Player, Creature * p_Creature)
-    {
-        if (!p_Player->HasQuest(Quests::Alliance_YourFirstBlacksmithingWorkOrder) && !p_Player->IsQuestRewarded(Quests::Alliance_YourFirstBlacksmithingWorkOrder))
-            p_Player->PlayerTalkClass->GetQuestMenu().AddMenuItem(Quests::Alliance_YourFirstBlacksmithingWorkOrder, 4);
-
-        p_Player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "I need you to do something for me.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF);
-        p_Player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, p_Creature->GetGUID());
-
-        return true;
-    }
-
-    //////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////
-
-    /// Called when a CreatureAI object is needed for the creature.
-    /// @p_Creature : Target creature instance
-    CreatureAI * npc_AuriaIrondreamer::GetAI(Creature * p_Creature) const
-    {
-        return new npc_AuriaIrondreamerAI(p_Creature);
-    }
-
-    //////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////
-
-    /// Constructor
-    npc_AuriaIrondreamer::npc_AuriaIrondreamerAI::npc_AuriaIrondreamerAI(Creature * p_Creature)
-        : GarrisonNPCAI(p_Creature)
-    {
-        SetAIObstacleManagerEnabled(true);
-        SetupActionSequence(AuriaIrondreamer_Level1::MovePointLoc, AuriaIrondreamer_Level1::Sequence, sizeof(AuriaIrondreamer_Level1::Sequence), AuriaIrondreamer_Level1::MovePointIDs::ForgeOre);
-
-        m_OnPointReached[AuriaIrondreamer_Level1::MovePointIDs::ForgeOre] = [this]() -> void
+        InitSequenceFunction FnLevel1 = [](GarrisonNPCAI * p_This, Creature * p_Me)
         {
-            AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::ForgeOre, [this]() -> void { DoNextSequenceAction(); });
-            m_DelayedOperations.push([this]() -> void { SetFacingBuildingRelative(AuriaIrondreamer_Level1::MovePointLoc[AuriaIrondreamer_Level1::MovePointIDs::ForgeOre - AuriaIrondreamer_Level1::MovePointIDs::ForgeOre].O); });
+            p_This->SetAIObstacleManagerEnabled(true);
+            p_This->SetupActionSequence(AuriaIrondreamer_Level1::MovePointLoc, AuriaIrondreamer_Level1::Sequence, sizeof(AuriaIrondreamer_Level1::Sequence), AuriaIrondreamer_Level1::MovePointIDs::ForgeOre);
 
-            AddTimedDelayedOperation(0 * IN_MILLISECONDS,                                  [this]() -> void { me->SetUInt32Value(UNIT_FIELD_ANIM_TIER, 8); });
-            AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::ForgeOre, [this]() -> void { me->SetUInt32Value(UNIT_FIELD_ANIM_TIER, 0); });
-        };
-
-        m_OnPointReached[AuriaIrondreamer_Level1::MovePointIDs::ForgeFront] = [this]() -> void
-        {
-            AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::ForgeFront, [this]() -> void { DoNextSequenceAction(); });
-            m_DelayedOperations.push([this]() -> void { SetFacingBuildingRelative(AuriaIrondreamer_Level1::MovePointLoc[AuriaIrondreamer_Level1::MovePointIDs::ForgeFront - AuriaIrondreamer_Level1::MovePointIDs::ForgeFront].O); });
-
-            AddTimedDelayedOperation(0 * IN_MILLISECONDS,                                    [this]() -> void { me->HandleEmoteCommand(EMOTE_STATE_READ_AND_TALK);    });
-            AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::ForgeFront, [this]() -> void
+            p_This->m_OnPointReached[AuriaIrondreamer_Level1::MovePointIDs::ForgeOre] = [p_This, p_Me]() -> void
             {
-                me->HandleEmoteCommand(0);
-                me->SetUInt32Value(UNIT_FIELD_EMOTE_STATE, 0);
-            });
+                p_This->AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::ForgeOre, [p_This]() -> void { p_This->DoNextSequenceAction(); });
+                p_This->m_DelayedOperations.push(                                                      [p_This]() -> void { p_This->SetFacingBuildingRelative(AuriaIrondreamer_Level1::MovePointLoc[AuriaIrondreamer_Level1::MovePointIDs::ForgeOre - AuriaIrondreamer_Level1::MovePointIDs::ForgeOre].O); });
+
+                p_This->AddTimedDelayedOperation(0 * IN_MILLISECONDS,                                  [p_Me]() -> void { p_Me->SetUInt32Value(UNIT_FIELD_ANIM_TIER, 8); });
+                p_This->AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::ForgeOre, [p_Me]() -> void { p_Me->SetUInt32Value(UNIT_FIELD_ANIM_TIER, 0); });
+            };
+
+            p_This->m_OnPointReached[AuriaIrondreamer_Level1::MovePointIDs::ForgeFront] = [p_This, p_Me]() -> void
+            {
+                p_This->AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::ForgeFront, [p_This]() -> void { p_This->DoNextSequenceAction(); });
+                p_This->m_DelayedOperations.push(                                                        [p_This]() -> void { p_This->SetFacingBuildingRelative(AuriaIrondreamer_Level1::MovePointLoc[AuriaIrondreamer_Level1::MovePointIDs::ForgeFront - AuriaIrondreamer_Level1::MovePointIDs::ForgeFront].O); });
+
+                p_This->AddTimedDelayedOperation(0 * IN_MILLISECONDS,                                    [p_Me]() -> void { p_Me->HandleEmoteCommand(EMOTE_STATE_READ_AND_TALK); });
+                p_This->AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::ForgeFront, [p_Me]() -> void
+                {
+                    p_Me->HandleEmoteCommand(0);
+                    p_Me->SetUInt32Value(UNIT_FIELD_EMOTE_STATE, 0);
+                });
+            };
+
+            p_This->m_OnPointReached[AuriaIrondreamer_Level1::MovePointIDs::Workorder] = [p_This, p_Me]() -> void
+            {
+                p_This->AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::Workorder, [p_This]() -> void { p_This->DoNextSequenceAction(); });
+                p_This->m_DelayedOperations.push(                                                       [p_This]() -> void { p_This->SetFacingBuildingRelative(AuriaIrondreamer_Level1::MovePointLoc[AuriaIrondreamer_Level1::MovePointIDs::Workorder - AuriaIrondreamer_Level1::MovePointIDs::Workorder].O); });
+            };
+
+            p_This->m_OnPointReached[AuriaIrondreamer_Level1::MovePointIDs::WorkorderDown] = [p_This, p_Me]() -> void
+            {
+                p_This->AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::WorkorderDown, [p_This]() -> void { p_This->DoNextSequenceAction(); });
+                p_This->m_DelayedOperations.push(                                                           [p_This]() -> void { p_This->SetFacingBuildingRelative(AuriaIrondreamer_Level1::MovePointLoc[AuriaIrondreamer_Level1::MovePointIDs::WorkorderDown - AuriaIrondreamer_Level1::MovePointIDs::WorkorderDown].O); });
+
+                p_This->AddTimedDelayedOperation(0 * IN_MILLISECONDS,                                       [p_Me]() -> void { p_Me->SetUInt32Value(UNIT_FIELD_ANIM_TIER, 8); });
+                p_This->AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::WorkorderDown, [p_Me]() -> void { p_Me->SetUInt32Value(UNIT_FIELD_ANIM_TIER, 0); });
+            };
+
+            p_This->m_OnPointReached[AuriaIrondreamer_Level1::MovePointIDs::WorkorderFront] = [p_This, p_Me]() -> void
+            {
+                p_This->AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::WorkorderFront, [p_This]() -> void { p_This->DoNextSequenceAction(); });
+                p_This->m_DelayedOperations.push(                                                            [p_This]() -> void { p_This->SetFacingBuildingRelative(AuriaIrondreamer_Level1::MovePointLoc[AuriaIrondreamer_Level1::MovePointIDs::ForgeOre - AuriaIrondreamer_Level1::MovePointIDs::WorkorderFront].O); });
+            };
+
+            p_This->m_OnPointReached[AuriaIrondreamer_Level1::MovePointIDs::ChestLeft] = [p_This, p_Me]() -> void
+            {
+                p_This->AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::ChestLeft, [p_This]() -> void { p_This->DoNextSequenceAction(); });
+                p_This->m_DelayedOperations.push(                                                       [p_This]() -> void { p_This->SetFacingBuildingRelative(AuriaIrondreamer_Level1::MovePointLoc[AuriaIrondreamer_Level1::MovePointIDs::ForgeOre - AuriaIrondreamer_Level1::MovePointIDs::ChestLeft].O); });
+
+                p_This->AddTimedDelayedOperation(0 * IN_MILLISECONDS,                                   [p_Me]() -> void { p_Me->SetUInt32Value(UNIT_FIELD_ANIM_TIER, 8); });
+                p_This->AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::ChestLeft, [p_Me]() -> void { p_Me->SetUInt32Value(UNIT_FIELD_ANIM_TIER, 0); });
+            };
+
+            p_This->DoNextSequenceAction();
         };
 
-        m_OnPointReached[AuriaIrondreamer_Level1::MovePointIDs::Workorder] = [this]() -> void
+        InitSequenceFunction FnLevel2 = [](GarrisonNPCAI * p_This, Creature * p_Me)
         {
-            AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::Workorder, [this]() -> void { DoNextSequenceAction(); });
-            m_DelayedOperations.push([this]() -> void { SetFacingBuildingRelative(AuriaIrondreamer_Level1::MovePointLoc[AuriaIrondreamer_Level1::MovePointIDs::Workorder - AuriaIrondreamer_Level1::MovePointIDs::Workorder].O); });
+
         };
 
-        m_OnPointReached[AuriaIrondreamer_Level1::MovePointIDs::WorkorderDown] = [this]() -> void
+        InitSequenceFunction FnLevel3 = [](GarrisonNPCAI * p_This, Creature * p_Me)
         {
-            AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::WorkorderDown, [this]() -> void { DoNextSequenceAction(); });
-            m_DelayedOperations.push([this]() -> void { SetFacingBuildingRelative(AuriaIrondreamer_Level1::MovePointLoc[AuriaIrondreamer_Level1::MovePointIDs::WorkorderDown - AuriaIrondreamer_Level1::MovePointIDs::WorkorderDown].O); });
 
-            AddTimedDelayedOperation(0 * IN_MILLISECONDS,                                       [this]() -> void { me->SetUInt32Value(UNIT_FIELD_ANIM_TIER, 8); });
-            AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::WorkorderDown, [this]() -> void { me->SetUInt32Value(UNIT_FIELD_ANIM_TIER, 0); });
         };
 
-        m_OnPointReached[AuriaIrondreamer_Level1::MovePointIDs::WorkorderFront] = [this]() -> void
+        char ScriptName[] = "npc_AuriaIrondreamer_Garr";
+
+        std::vector<SkillNPC_RecipeEntry> Recipes
         {
-            AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::WorkorderFront, [this]() -> void { DoNextSequenceAction(); });
-            m_DelayedOperations.push([this]() -> void { SetFacingBuildingRelative(AuriaIrondreamer_Level1::MovePointLoc[AuriaIrondreamer_Level1::MovePointIDs::ForgeOre - AuriaIrondreamer_Level1::MovePointIDs::WorkorderFront].O); });
+            { 171692,     0 },
+            { 171693,     0 },
+            { 171691,     0 },
+            { 171694, 28366 },
+            { 171695, 28366 },
+            { 171696, 28366 },
+            { 171697, 28366 },
+            { 171698, 28366 },
+            { 171699, 28366 },
+            { 171700, 28367 },
+            { 171701, 28367 },
+            { 171702, 28367 },
+            { 171703, 28367 },
+            { 171704, 28367 },
+            { 171705, 28367 },
+            { 171706, 28367 },
+            { 171707, 28367 },
+            { 173355, 28366 },
+            { 178243, 28366 },
+            { 178245, 28366 },
         };
-
-        m_OnPointReached[AuriaIrondreamer_Level1::MovePointIDs::ChestLeft] = [this]() -> void
-        {
-            AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::ChestLeft, [this]() -> void { DoNextSequenceAction(); });
-            m_DelayedOperations.push([this]() -> void { SetFacingBuildingRelative(AuriaIrondreamer_Level1::MovePointLoc[AuriaIrondreamer_Level1::MovePointIDs::ForgeOre - AuriaIrondreamer_Level1::MovePointIDs::ChestLeft].O); });
-
-            AddTimedDelayedOperation(0 * IN_MILLISECONDS,                                   [this]() -> void { me->SetUInt32Value(UNIT_FIELD_ANIM_TIER, 8); });
-            AddTimedDelayedOperation(AuriaIrondreamer_Level1::DestPointDuration::ChestLeft, [this]() -> void { me->SetUInt32Value(UNIT_FIELD_ANIM_TIER, 0); });
-        };
-
-        DoNextSequenceAction();
     }
 
     //////////////////////////////////////////////////////////////////////////
     /// 77792 - Yulia Samras                                              ////
     //////////////////////////////////////////////////////////////////////////
-
-    /// Constructor
-    npc_YuliaSamras::npc_YuliaSamras()
-        : CreatureScript("npc_YuliaSamras_Garr")
+    namespace npc_YuliaSamrasAIData
     {
-
-    }
-
-    //////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////
-
-    /// Called when a player opens a gossip dialog with the GameObject.
-    /// @p_Player     : Source player instance
-    /// @p_Creature   : Target GameObject instance
-    bool npc_YuliaSamras::OnGossipHello(Player * p_Player, Creature * p_Creature)
-    {
-        if (p_Player->HasQuest(Quests::Alliance_YourFirstBlacksmithingWorkOrder) || p_Player->IsQuestRewarded(Quests::Alliance_YourFirstBlacksmithingWorkOrder))
-            p_Player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "I would like to place an order.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF);
-
-        if (p_Player->HasQuest(Quests::Alliance_YourFirstBlacksmithingWorkOrder) && !p_Player->IsQuestRewarded(Quests::Alliance_YourFirstBlacksmithingWorkOrder))
-            p_Player->PlayerTalkClass->GetQuestMenu().AddMenuItem(Quests::Alliance_YourFirstBlacksmithingWorkOrder, 4);
-
-        p_Player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, p_Creature->GetGUID());
-
-        return true;
-    }
-
-    /// Called when a player selects a gossip item in the creature's gossip menu.
-    /// @p_Player   : Source player instance
-    /// @p_Creature : Target creature instance
-    /// @p_Sender   : Sender menu
-    /// @p_Action   : Action
-    bool npc_YuliaSamras::OnGossipSelect(Player * p_Player, Creature * p_Creature, uint32 p_Sender, uint32 p_Action)
-    {
-        if (p_Player && p_Creature && p_Creature->AI() && p_Creature->GetScriptName() == GetName())
-            reinterpret_cast<GarrisonNPCAI*>(p_Creature->AI())->SendShipmentCrafterUI(p_Player);
-
-        return true;
-    }
-
-    //////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////
-
-    /// Called when a CreatureAI object is needed for the creature.
-    /// @p_Creature : Target creature instance
-    CreatureAI * npc_YuliaSamras::GetAI(Creature * p_Creature) const
-    {
-        return new npc_YuliaSamrasAI(p_Creature);
-    }
-
-    //////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////
-
-    /// Constructor
-    npc_YuliaSamras::npc_YuliaSamrasAI::npc_YuliaSamrasAI(Creature * p_Creature)
-        : GarrisonNPCAI(p_Creature)
-    {
-        SetAIObstacleManagerEnabled(true);
-        SetupActionSequence(YuliaSamras_Level1::MovePointLoc, YuliaSamras_Level1::Sequence, sizeof(YuliaSamras_Level1::Sequence), YuliaSamras_Level1::MovePointIDs::Workorder);
-
-        m_OnPointReached[YuliaSamras_Level1::MovePointIDs::Workorder] = [this]() -> void
+        InitSequenceFunction FnLevel1 = [](GarrisonNPCAI * p_This, Creature * p_Me)
         {
-            AddTimedDelayedOperation(YuliaSamras_Level1::DestPointDuration::Workorder, [this]() -> void { DoNextSequenceAction(); });
-            m_DelayedOperations.push([this]() -> void { SetFacingBuildingRelative(YuliaSamras_Level1::MovePointLoc[YuliaSamras_Level1::MovePointIDs::Workorder - YuliaSamras_Level1::MovePointIDs::Workorder].O); });
+            p_This->SetAIObstacleManagerEnabled(true);
+            p_This->SetupActionSequence(YuliaSamras_Level1::MovePointLoc, YuliaSamras_Level1::Sequence, sizeof(YuliaSamras_Level1::Sequence), YuliaSamras_Level1::MovePointIDs::Workorder);
+
+            p_This->m_OnPointReached[YuliaSamras_Level1::MovePointIDs::Workorder] = [p_This]() -> void
+            {
+                p_This->AddTimedDelayedOperation(YuliaSamras_Level1::DestPointDuration::Workorder, [p_This]() -> void { p_This->DoNextSequenceAction(); });
+                p_This->m_DelayedOperations.push(                                                  [p_This]() -> void { p_This->SetFacingBuildingRelative(YuliaSamras_Level1::MovePointLoc[YuliaSamras_Level1::MovePointIDs::Workorder - YuliaSamras_Level1::MovePointIDs::Workorder].O); });
+            };
+
+            p_This->DoNextSequenceAction();
         };
 
-        DoNextSequenceAction();
+        InitSequenceFunction FnLevel2 = [](GarrisonNPCAI * p_This, Creature * p_Me)
+        {
+
+        };
+
+        InitSequenceFunction FnLevel3 = [](GarrisonNPCAI * p_This, Creature * p_Me)
+        {
+
+        };
+
+        char ScriptName[] = "npc_YuliaSamras_Garr";
     }
 
 }   ///< namespace Garrison
