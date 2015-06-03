2c8bbb266c9663b6325ef9d866f310e31a5bdec0
twink <nikazhazha@mail.ru>
2015-06-03 21:14:51 +0300

    Fix Enrage proc on absorb.

--- sql/updates/world/2015_06_10_world_spellfixes_twink.sql
+++ sql/updates/world/2015_06_10_world_spellfixes_twink.sql
@@ -10,3 +10,7 @@ INSERT INTO `spell_script_names` VALUES (159195, 'spell_mastery_primal_tenacity'
 -- fix usebug with Carrying Seaforium
 DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_gen_carrying_seaforium';
 INSERT INTO `spell_script_names` VALUES (52410, 'spell_gen_carrying_seaforium');
+
+-- fix Enrage proc on absorb
+DELETE FROM `spell_proc_event` WHERE `entry`=13046;
+INSERT INTO `spell_proc_event` (`entry`, `procEx`) VALUES (13046, 0x0000002 + 0x0000400);
