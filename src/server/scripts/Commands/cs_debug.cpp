dfd7f0060136c58637c047540227e5d1991cf80c
Izidor <millenium_woweur@hotmail.fr>
2015-06-08 16:42:45 +0200

    Script/Command: Update premade generation for PvE PTR premades.

--- src/server/scripts/Commands/cs_debug.cpp
+++ src/server/scripts/Commands/cs_debug.cpp
@@ -2574,17 +2574,21 @@ class debug_commandscript: public CommandScript
             const uint32 l_AllianceMask = 0xFD7FFC4D;
             std::string l_SearchString = p_Args; /// Case sensitive search
 
-            std::list<uint8> l_ClassWeaponFind;
+            std::list<uint8> l_ArmorSlotFind;
+            std::list<uint32> l_ClassWeaponFind;
             std::list<uint8> l_CloaksFind;
             std::map<uint8, uint8> l_TrinketsFind;
 
             
             l_FirstEntry = true;
-            l_StrBuilder << "INSERT INTO character_template_item VALUES";
+            l_StrBuilder << "INSERT INTO character_template_item (id, itemID, faction, count) VALUES ";
             ItemTemplateContainer const* l_Store = sObjectMgr->GetItemTemplateStore();
             for (ItemTemplateContainer::const_iterator l_Iter = l_Store->begin(); l_Iter != l_Store->end(); ++l_Iter)
             {
                 ItemTemplate const* l_Template = &l_Iter->second;
+
+                if (sSpellMgr->GetItemSourceSkills(l_Template->ItemId) != nullptr)
+                    continue;
 
                 if (l_Template->InventoryType == INVTYPE_TRINKET)
                 {
@@ -2599,7 +2603,7 @@ class debug_commandscript: public CommandScript
                             if (l_TrinketsFind.find(l_ClassId) != l_TrinketsFind.end() && l_TrinketsFind[l_ClassId] == 2)
                                 continue;
 
-                            if (l_Template->ItemLevel != 610)
+                            if (l_Template->ItemLevel != 640)
                                 continue;
 
                             if (l_TrinketsFind.find(l_ClassId) == l_TrinketsFind.end())
@@ -2626,7 +2630,7 @@ class debug_commandscript: public CommandScript
                             if (std::find(l_CloaksFind.begin(), l_CloaksFind.end(), l_ClassId) != l_CloaksFind.end())
                                 continue;
 
-                            if (l_Template->ItemLevel != 610)
+                            if (l_Template->ItemLevel != 640)
                                 continue;
 
                             l_CloaksFind.push_back(l_ClassId);
@@ -2649,13 +2653,13 @@ class debug_commandscript: public CommandScript
                             if (!l_Template->HasClassSpec(l_ClassId))
                                 continue;
 
-                            if (std::find(l_ClassWeaponFind.begin(), l_ClassWeaponFind.end(), l_ClassId) != l_ClassWeaponFind.end())
+                            if (std::find(l_ClassWeaponFind.begin(), l_ClassWeaponFind.end(), l_ClassId | l_Template->SubClass << 16) != l_ClassWeaponFind.end())
                                 continue;
 
-                            if (l_Template->ItemLevel != 610)
+                            if (l_Template->ItemLevel != 640)
                                 continue;
 
-                            l_ClassWeaponFind.push_back(l_ClassId);
+                            l_ClassWeaponFind.push_back(l_ClassId | l_Template->SubClass << 16);
 
                             l_Count = l_Template->IsOneHanded() || (l_Template->IsTwoHandedWeapon() && l_ClassId == CLASS_WARRIOR) ? 2 : 1;
                             l_StrBuilder << (l_FirstEntry ? "" : ",") << std::endl << "(" << l_ClassId << " ," << l_Template->ItemId << " ," <<  1 << " ," << l_Count << ")", l_FirstEntry = false;
@@ -2665,7 +2669,7 @@ class debug_commandscript: public CommandScript
                 }
 
                 /// Armor
-                if (std::string(l_Template->Name1->Get(LOCALE_enUS)).find(l_SearchString) != std::string::npos)
+                //if (std::string(l_Template->Name1->Get(LOCALE_enUS)).find(l_SearchString) != std::string::npos)
                 {
                     if (l_Template->Class != ITEM_CLASS_ARMOR)
                         continue;
@@ -2730,19 +2734,12 @@ class debug_commandscript: public CommandScript
                                 }
                             }
 
-                            if (l_Template->Class == ITEM_CLASS_WEAPON)
-                            {
-                                if (!l_Template->HasClassSpec(l_ClassId))
-                                    continue;
-
-                                if (std::find(l_ClassWeaponFind.begin(), l_ClassWeaponFind.end(), l_ClassId) != l_ClassWeaponFind.end())
-                                    continue;
-
-                                l_ClassWeaponFind.push_back(l_ClassId);
-                            }
+                            if (!l_Template->HasClassSpec(l_ClassId) || l_Template->ItemLevel != 640)
+                                continue;
 
                             l_Count = 1;
-                            l_StrBuilder << (l_FirstEntry ? "" : ",") << std::endl << "(" << l_ClassId << " ," << l_Template->ItemId << " ," << l_TeamIndex + 1 << " ," << l_Count << ")", l_FirstEntry = false;
+                            l_StrBuilder << (l_FirstEntry ? "" : ",") << std::endl << "(" << l_ClassId << " ," << l_Template->ItemId << " ," <<  1 << " ," << l_Count << ")", l_FirstEntry = false;
+                            l_StrBuilder << (l_FirstEntry ? "" : ",") << std::endl << "(" << l_ClassId << " ," << l_Template->ItemId << " ," << 2 << " ," << l_Count << ")", l_FirstEntry = false;
                         }
                     }
                 }
