a1a75a106509cd06408553d26fe8af893bae6d05
pinkmouse <justin.mariette@gmail.com>
2015-06-17 19:30:33 -0500

    Fix Sql error

--- sql/updates/world/2015_06_17_world_spell_script_names_soul_leech.sql
+++ sql/updates/world/2015_06_17_world_spell_script_names_soul_leech.sql
@@ -5,7 +5,5 @@ INSERT INTO `spell_script_names` VALUES (686, 'spell_warl_soul_leech');
 INSERT INTO `spell_script_names` VALUES (6353, 'spell_warl_soul_leech');
 INSERT INTO `spell_script_names` VALUES (104027, 'spell_warl_soul_leech');
 INSERT INTO `spell_script_names` VALUES (103964, 'spell_warl_soul_leech');
-INSERT INTO `spell_script_names` VALUES (6353, 'spell_warl_soul_leech');
-INSERT INTO `spell_script_names` VALUES (104027, 'spell_warl_soul_leech');
 INSERT INTO `spell_script_names` VALUES (29722, 'spell_warl_soul_leech');
 INSERT INTO `spell_script_names` VALUES (17877, 'spell_warl_soul_leech');
