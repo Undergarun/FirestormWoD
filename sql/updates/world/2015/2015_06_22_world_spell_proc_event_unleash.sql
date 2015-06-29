66a64d56efac49b6f48de649351de50a18ea7dcd
Merydwin <merydwin@gmail.com>
2015-06-29 10:41:01 +0200

    Core/SQL Fix typo

--- sql/updates/world/2015/2015_06_22_world_spell_proc_event_unleash.sql
+++ sql/updates/world/2015/2015_06_22_world_spell_proc_event_unleash.sql
@@ -1,2 +1,2 @@
 DELETE FROM spell_proc_event WHERE entry = 73681;
-INSERT INTO spelL_proc_event (entry, procFlags) VALUE (73681, 4);
+INSERT INTO spell_proc_event (entry, procFlags) VALUE (73681, 4);
