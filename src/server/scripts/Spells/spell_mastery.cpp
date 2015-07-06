150d0d688789f694f7004def47d892f4d89c50b7
pinkmouse <justin.mariette@gmail.com>
2015-07-06 16:09:22 -0500

    Spells/Rogue: No more need a script for Mastery: Potent Poisons

--- src/server/scripts/Spells/spell_mastery.cpp
+++ src/server/scripts/Spells/spell_mastery.cpp
@@ -276,38 +276,6 @@ class spell_mastery_executioner : public SpellScriptLoader
         }
 };
 
-/// Mastery: Potent Poisons - 76803
-class spell_mastery_potent_poisons : public SpellScriptLoader
-{
-    public:
-        spell_mastery_potent_poisons() : SpellScriptLoader("spell_mastery_potent_poisons") { }
-
-        class spell_mastery_potent_poisons_AuraScript : public AuraScript
-        {
-            PrepareAuraScript(spell_mastery_potent_poisons_AuraScript);
-
-            void CalculateAmount(constAuraEffectPtr, int32& p_Amount, bool&)
-            {
-                if (Unit* l_Caster = GetCaster())
-                {
-                    float l_Mastery = l_Caster->GetFloatValue(PLAYER_FIELD_MASTERY) * (float(GetSpellInfo()->Effects[EFFECT_2].BasePoints) / 100.0f);
-                    p_Amount = l_Mastery;
-                }
-            }
-
-            void Register()
-            {
-                DoEffectCalcAmount += AuraEffectCalcAmountFn(spell_mastery_potent_poisons_AuraScript::CalculateAmount, EFFECT_0, SPELL_AURA_ADD_PCT_MODIFIER);
-                DoEffectCalcAmount += AuraEffectCalcAmountFn(spell_mastery_potent_poisons_AuraScript::CalculateAmount, EFFECT_1, SPELL_AURA_ADD_PCT_MODIFIER);
-            }
-        };
-
-        AuraScript* GetAuraScript() const
-        {
-            return new spell_mastery_potent_poisons_AuraScript();
-        }
-};
-
 /// Mastery: Sniper Training - 76659
 class spell_mastery_sniper_training : public SpellScriptLoader
 {
@@ -1254,7 +1222,6 @@ void AddSC_mastery_spell_scripts()
     new spell_mastery_molten_earth_damage();
     new spell_mastery_razor_claws();
     new spell_mastery_executioner();
-    new spell_mastery_potent_poisons();
     new spell_mastery_sniper_training();
     new spell_mastery_recently_moved();
     new spell_mastery_sniper_training_aura();
