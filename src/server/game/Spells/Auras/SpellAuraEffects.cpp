9f191e8a1cd68c825433663af571950eb0017756
Izidor <millenium_woweur@hotmail.fr>
2015-07-24 13:59:59 +0200

    Core/Spell: Fix insane rupture damage.

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -888,7 +888,7 @@ int32 AuraEffect::CalculateAmount(Unit* caster)
                     float l_AttackPower = caster->GetTotalAttackPowerValue(WeaponAttackType::BaseAttack);
 
                     /// 1 tick = Combo Points * (0.0685 * Attack power) * 0.5
-                    amount += l_Combo * int32(l_AttackPower * 0.5f) * (m_periodicTimer / IN_MILLISECONDS);
+                    amount += l_Combo * int32(l_AttackPower * 0.0685f); ///< last update 6.1.2 Build 19865
                 }
                 case 50536: // Unholy Blight
                 {
