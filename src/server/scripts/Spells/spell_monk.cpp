3de6b1ae8804398e8ab4c5bc754753f2783d37f6
pinkmouse <justin.mariette@gmail.com>
2015-08-04 16:56:09 -0500

    Spells/Monk: Try to fix a bug with Eminence targets

--- src/server/scripts/Spells/spell_monk.cpp
+++ src/server/scripts/Spells/spell_monk.cpp
@@ -1524,7 +1524,7 @@ class spell_monk_eminence_heal : public SpellScriptLoader
             {
                 if ((*l_Itr) == nullptr || (*l_Itr)->ToUnit() == nullptr || !(*l_Itr)->ToUnit()->IsInRaidWith(l_Caster) || 
                     ((*l_Itr)->ToUnit()->GetGUID() == l_Caster->GetGUID() && GetSpellInfo()->Id != SPELL_MONK_EMINENCE_HEAL) ||
-                    !l_Caster->IsValidAssistTarget((*l_Itr)->ToUnit()))
+                    l_Caster->IsHostileTo((*l_Itr)->ToUnit()))
                     l_Itr = p_Targets.erase(l_Itr);
                else
                    l_Itr++;
