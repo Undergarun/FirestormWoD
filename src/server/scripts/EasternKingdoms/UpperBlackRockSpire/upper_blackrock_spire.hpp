5617f605e3cf8ab4df772c3cbdb52a1b00ddb8b0
Morgoporc <j.fraisse45@laposte.net>
2015-08-27 10:24:44 +0200

    Core/Scripts : Refactor files organization for Upper Blackrock Spire
    Signed-off-by: Morgoporc <j.fraisse45@laposte.net>

--- src/server/scripts/EasternKingdoms/UpperBlackRockSpire/upper_blackrock_spire.hpp
+++ src/server/scripts/EasternKingdoms/UpperBlackRockSpire/upper_blackrock_spire.hpp
@@ -1,24 +1,25 @@
-/*
-* Copyright (C) 2012-2014 JadeCore <http://www.pandashan.com>
-* Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
-* Copyright (C) 2006-2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
-*
-* This program is free software; you can redistribute it and/or modify it
-* under the terms of the GNU General Public License as published by the
-* Free Software Foundation; either version 2 of the License, or (at your
-* option) any later version.
-*
-* This program is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-* more details.
-*
-* You should have received a copy of the GNU General Public License along
-* with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+////////////////////////////////////////////////////////////////////////////////
+///
+///  MILLENIUM-STUDIO
+///  Copyright 2015 Millenium-studio SARL
+///  All Rights Reserved.
+///
+////////////////////////////////////////////////////////////////////////////////
 
-#ifndef UPPER_BLACKROCK_SPIRE_H
-# define UPPER_BLACKROCK_SPIRE_H
+#ifndef UPPER_BLACKROCK_SPIRE_HPP
+# define UPPER_BLACKROCK_SPIRE_HPP
+
+# include "ScriptPCH.h"
+# include "ObjectAccessor.h"
+# include "ScriptMgr.h"
+# include "ScriptedCreature.h"
+# include "ObjectMgr.h"
+# include "SpellScript.h"
+# include "MoveSplineInit.h"
+# include "Vehicle.h"
+# include "Language.h"
+# include "ScriptedEscortAI.h"
+# include "GameObjectAI.h"
 
 enum eDatas
 {
@@ -154,4 +155,4 @@ enum eWorldStates
     WorldStateEnableChicken = 9523
 };
 
-#endif
+#endif  ///< UPPER_BLACKROCK_SPIRE_HPP
