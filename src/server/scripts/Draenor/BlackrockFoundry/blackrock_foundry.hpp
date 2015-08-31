b54254075cac186245b1cdb7410e953db49796e3
Morgoporc <j.fraisse45@laposte.net>
2015-08-31 16:49:14 +0200

    Scripts/BlackrockFoundry : Add prepared files for Blackrock Foundry
    Signed-off-by: Morgoporc <j.fraisse45@laposte.net>

---
+++ src/server/scripts/Draenor/BlackrockFoundry/blackrock_foundry.hpp
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////////////////////
+///
+///  MILLENIUM-STUDIO
+///  Copyright 2015 Millenium-studio SARL
+///  All Rights Reserved.
+///
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef BLACKROCK_FOUNDRY_HPP
+# define BLACKROCK_FOUNDRY_HPP
+
+# include "InstanceScript.h"
+# include "ScriptedCosmeticAI.hpp"
+# include "ScriptedCreature.h"
+# include "ScriptMgr.h"
+# include "ScriptUtils.h"
+# include "Cell.h"
+# include "CellImpl.h"
+# include "GridNotifiers.h"
+# include "GridNotifiersImpl.h"
+# include "Vehicle.h"
+# include "GameObjectAI.h"
+# include "Group.h"
+# include "MoveSplineInit.h"
+
+#endif
