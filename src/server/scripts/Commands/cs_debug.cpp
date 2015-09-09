2a43afe17824658dcc9be4e353541e8d3db4c141
Tuxity <kevin.darcel@gmail.com>
2015-09-09 12:20:15 -0500

    Scripts/Cmd: Improve command to generate char templates

--- src/server/scripts/Commands/cs_debug.cpp
+++ src/server/scripts/Commands/cs_debug.cpp
@@ -2582,36 +2582,213 @@ class debug_commandscript: public CommandScript
             l_StrBuilder << "DELETE FROM character_template;" << std::endl;
             l_StrBuilder << "DELETE FROM character_template_item;" << std::endl;
             l_StrBuilder << "DELETE FROM character_template_spell;" << std::endl;
-            l_StrBuilder << "DELETE FROM character_template_reputation;" << std::endl;
-            
+            l_StrBuilder << "DELETE FROM character_template_reputation;" << std::endl << std::endl;
+
             bool l_FirstEntry = true;
             l_StrBuilder << "INSERT INTO character_template VALUES";
             for (int32 l_ClassId = CLASS_WARRIOR; l_ClassId < MAX_CLASSES; l_ClassId++)
             {
                 ChrClassesEntry const* l_ClassEntry = sChrClassesStore.LookupEntry(l_ClassId);
-                l_StrBuilder << (l_FirstEntry ? "" : ",") << std::endl << "(" << l_ClassId << " ," << l_ClassId << " ,\"" << l_ClassEntry->NameLang << " - Level 100\", \"\", 100, " \
-                    << "100000000, -8833.07, 622.778, 93.9317, 0.6771, 0, 1569.97, -4397.41, 16.0472, 0.543025, 1, 0)", l_FirstEntry = false;
+                l_StrBuilder << (l_FirstEntry ? "" : ",") << std::endl
+                             << "("
+                             << l_ClassId << ", "
+                             << l_ClassId << ", "
+                             << "\"" << l_ClassEntry->NameLang << " - Level 100\", \"\", 100, "
+                             << "100000000, -8833.07, 622.778, 93.9317, 0.6771, 0, 1569.97, -4397.41, 16.0472, 0.543025, 1, 0)";
+
+                l_FirstEntry = false;
             }
-            
             l_StrBuilder << ";" << std::endl << std::endl;
 
             const uint32 l_HordeMask = 0xFE5FFBB2;
             const uint32 l_AllianceMask = 0xFD7FFC4D;
+            const uint32 l_ItemLevel = 670;
+            const uint32 l_BagItemId = 114821;
+
             std::string l_SearchString = p_Args; /// Case sensitive search
-            
+
+            std::list<uint8> l_ArmorSlotFind;
+            std::list<uint32> l_ClassWeaponFind;
+            std::list<uint8> l_CloaksFind;
+            std::map<uint8, uint8> l_TrinketsFind;
+
             l_FirstEntry = true;
-            l_StrBuilder << "INSERT INTO character_template_item VALUES";
+            l_StrBuilder << "INSERT INTO character_template_item (id, itemID, faction, count) VALUES ";
             ItemTemplateContainer const* l_Store = sObjectMgr->GetItemTemplateStore();
             for (ItemTemplateContainer::const_iterator l_Iter = l_Store->begin(); l_Iter != l_Store->end(); ++l_Iter)
             {
                 ItemTemplate const* l_Template = &l_Iter->second;
-                if (std::string(l_Template->Name1->Get(LOCALE_enUS)).find(l_SearchString) != std::string::npos)
+
+                if (l_Template->InventoryType == INVTYPE_BAG)
                 {
-                    if (l_Template->Class != ITEM_CLASS_ARMOR && l_Template->Class != ITEM_CLASS_WEAPON)
+                    uint32 l_Count = 4;
+
+                    if (l_Template->ItemId != l_BagItemId)
                         continue;
 
-                    int32 l_TeamIndex = l_Template->AllowableRace == l_HordeMask;
+                    for (int32 l_ClassId = CLASS_WARRIOR; l_ClassId < MAX_CLASSES; l_ClassId++)
+                    {
+                        int32 l_ClassMask = 1 << (l_ClassId - 1);
+                        if (l_Template->AllowableClass & l_ClassMask)
+                        {
+                            l_StrBuilder << (l_FirstEntry ? "" : ",") << std::endl
+                                         << "("
+                                         << l_ClassId << ", "
+                                         << l_BagItemId << ", "
+                                         << "1, "
+                                         << l_Count
+                                         << ")";
+                            l_StrBuilder << "," << std::endl
+                                         << "("
+                                         << l_ClassId << ", "
+                                         << l_BagItemId << ", "
+                                         << "2, "
+                                         << l_Count
+                                         << ")";
+
+                            l_FirstEntry = false;
+                        }
+                    }
+                }
+
+                /// Must after bags
+                if (sSpellMgr->GetItemSourceSkills(l_Template->ItemId) != nullptr)
+                    continue;
+
+                if (l_Template->InventoryType == INVTYPE_TRINKET)
+                {
                     uint32 l_Count = 1;
+
+                    if (l_Template->ItemLevel != l_ItemLevel)
+                        continue;
+
+                    for (int32 l_ClassId = CLASS_WARRIOR; l_ClassId < MAX_CLASSES; l_ClassId++)
+                    {
+                        int32 l_ClassMask = 1 << (l_ClassId - 1);
+                        if (l_Template->AllowableClass & l_ClassMask)
+                        {
+                            if (!l_Template->HasClassSpec(l_ClassId))
+                                continue;
+
+                            if (l_TrinketsFind.find(l_ClassId) != l_TrinketsFind.end() && l_TrinketsFind[l_ClassId] == 2)
+                                continue;
+
+                            if (l_TrinketsFind.find(l_ClassId) == l_TrinketsFind.end())
+                                l_TrinketsFind[l_ClassId] = 1;
+                            else
+                                l_TrinketsFind[l_ClassId] = 2;
+
+                            l_StrBuilder << (l_FirstEntry ? "" : ",") << std::endl
+                                         << "("
+                                         << l_ClassId << ", "
+                                         << l_Template->ItemId << ", "
+                                         << "1, "
+                                         << l_Count
+                                         << ")";
+                            l_StrBuilder << "," << std::endl
+                                         << "("
+                                         << l_ClassId << ", "
+                                         << l_Template->ItemId << ", "
+                                         << "2, "
+                                         << l_Count
+                                         << ")";
+
+                             l_FirstEntry = false;
+                        }
+                    }
+                }
+
+                if (l_Template->InventoryType == INVTYPE_CLOAK)
+                {
+                    uint32 l_Count = 1;
+
+                    if (l_Template->ItemLevel != l_ItemLevel)
+                        continue;
+
+                    for (int32 l_ClassId = CLASS_WARRIOR; l_ClassId < MAX_CLASSES; l_ClassId++)
+                    {
+                        int32 l_ClassMask = 1 << (l_ClassId - 1);
+                        if (l_Template->AllowableClass & l_ClassMask)
+                        {
+                            if (!l_Template->HasClassSpec(l_ClassId))
+                                continue;
+
+                            if (std::find(l_CloaksFind.begin(), l_CloaksFind.end(), l_ClassId) != l_CloaksFind.end())
+                                continue;
+
+                            l_CloaksFind.push_back(l_ClassId);
+
+                            l_StrBuilder << (l_FirstEntry ? "" : ",") << std::endl
+                                         << "("
+                                         << l_ClassId << ", "
+                                         << l_Template->ItemId << ", "
+                                         << "1, "
+                                         << l_Count
+                                         << ")";
+                            l_StrBuilder << "," << std::endl
+                                         << "("
+                                         << l_ClassId << ", "
+                                         << l_Template->ItemId << ", "
+                                         << "2, "
+                                         << l_Count
+                                         << ")";
+
+                            l_FirstEntry = false;
+                        }
+                    }
+                }
+
+                if (l_Template->Class == ITEM_CLASS_WEAPON)
+                {
+                    uint32 l_Count = 1;
+
+                    if (l_Template->ItemLevel != l_ItemLevel)
+                        continue;
+
+                    for (int32 l_ClassId = CLASS_WARRIOR; l_ClassId < MAX_CLASSES; l_ClassId++)
+                    {
+                        int32 l_ClassMask = 1 << (l_ClassId - 1);
+                        if (l_Template->AllowableClass & l_ClassMask)
+                        {
+                            if (!l_Template->HasClassSpec(l_ClassId))
+                                continue;
+
+                            if (std::find(l_ClassWeaponFind.begin(), l_ClassWeaponFind.end(), l_ClassId | l_Template->SubClass << 16) != l_ClassWeaponFind.end())
+                                continue;
+
+                            l_ClassWeaponFind.push_back(l_ClassId | l_Template->SubClass << 16);
+
+                            l_Count = l_Template->IsOneHanded() || (l_Template->IsTwoHandedWeapon() && l_ClassId == CLASS_WARRIOR) ? 2 : 1;
+                            l_StrBuilder << (l_FirstEntry ? "" : ",") << std::endl
+                                         << "("
+                                         << l_ClassId << ", "
+                                         << l_Template->ItemId << ", "
+                                         <<  "1, "
+                                         << l_Count
+                                         << ")";
+                            l_StrBuilder << "," << std::endl
+                                         << "("
+                                         << l_ClassId << ", "
+                                         << l_Template->ItemId << ", "
+                                         << "2, "
+                                         << l_Count
+                                         << ")";
+
+                            l_FirstEntry = false;
+                        }
+                    }
+                }
+
+                /// Armor
+                //if (std::string(l_Template->Name1->Get(LOCALE_enUS)).find(l_SearchString) != std::string::npos)
+                {
+                    if (l_Template->Class != ITEM_CLASS_ARMOR)
+                        continue;
+
+                    uint32 l_Count = 1;
+
+                    if (l_Template->ItemLevel != l_ItemLevel)
+                        continue;
 
                     for (int32 l_ClassId = CLASS_WARRIOR; l_ClassId < MAX_CLASSES; l_ClassId++)
                     {
@@ -2670,13 +2847,29 @@ class debug_commandscript: public CommandScript
                                 }
                             }
 
-                            l_Count = l_Template->IsOneHanded() || (l_Template->IsTwoHandedWeapon() && l_ClassId == CLASS_WARRIOR) ? 2 : 1;
-                            l_StrBuilder << (l_FirstEntry ? "" : ",") << std::endl << "(" << l_ClassId << " ," << l_Template->ItemId << " ," << l_TeamIndex + 1 << " ," << l_Count << ")", l_FirstEntry = false;
+                            if (!l_Template->HasClassSpec(l_ClassId))
+                                continue;
+
+                            l_StrBuilder << (l_FirstEntry ? "" : ",") << std::endl
+                                         << "("
+                                         << l_ClassId << ", "
+                                         << l_Template->ItemId << ", "
+                                         <<  "1, "
+                                         << l_Count
+                                         << ")";
+                            l_StrBuilder << "," << std::endl
+                                         << "("
+                                         << l_ClassId << ", "
+                                         << l_Template->ItemId << ", "
+                                         << "2, "
+                                         << l_Count
+                                        << ")";
+
+                            l_FirstEntry = false;
                         }
                     }
                 }
             }
-
 
             l_StrBuilder << ";" << std::endl;
             std::string l_FinalString = l_StrBuilder.str();
@@ -2684,6 +2877,9 @@ class debug_commandscript: public CommandScript
             fwrite(l_FinalString.c_str(), l_FinalString.length(), 1, l_Output);
             fflush(l_Output);
             fclose(l_Output);
+
+            p_Handler->PSendSysMessage("Characters templates generated !");
+
             return true;
         }
 
