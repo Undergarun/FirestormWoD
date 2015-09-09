47d1d44d2478d6078f1cd5a2a1848d0a1ccbfef5
Sovak <sovak007@gmail.com>
2015-09-09 16:32:09 +0200

    Core/Garrison: Fixed a crash in mission distribution
    Signed-off-by: Sovak <sovak007@gmail.com>

--- src/server/game/Garrison/GarrisonMgr.cpp
+++ src/server/game/Garrison/GarrisonMgr.cpp
@@ -3643,7 +3643,10 @@ namespace MS { namespace Garrison
                     {
                         GarrMissionRewardEntry const* l_RewardEntry = sGarrMissionRewardStore.LookupEntry(l_RewardIT);
 
-                        if (l_RewardEntry && l_RewardEntry->MissionID == l_Entry->MissionRecID)
+                        if (!l_RewardEntry)
+                            continue;
+
+                        if (l_RewardEntry->MissionID == l_Entry->MissionRecID)
                             l_RewardCount++;
 
                         /// Elemental Rune & Abrogator Stone - Legendary Questline  NYI
