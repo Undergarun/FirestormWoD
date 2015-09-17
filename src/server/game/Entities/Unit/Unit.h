3523b298c07279c98760cc9374bd396bda152ef9
DPSLynix <lynix680@gmail.com>
2015-09-17 12:24:22 +0200

    Spells/Shaman: Fix Totemic Persistence - 108284

--- src/server/game/Entities/Unit/Unit.h
+++ src/server/game/Entities/Unit/Unit.h
@@ -1384,7 +1384,7 @@ enum ReactiveType
 #define MAX_TOTEM_SLOT      5
 #define SUMMON_SLOT_MINIPET 5
 #define SUMMON_SLOT_QUEST   6
-#define MAX_SUMMON_SLOT     7
+#define MAX_SUMMON_SLOT     9
 
 #define MAX_GAMEOBJECT_SLOT 4
 
