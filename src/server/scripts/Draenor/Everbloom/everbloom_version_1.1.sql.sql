c3e5a26cfe8998be5f4d36122a8844ea4219fa32
Daniel <dobidoberman1@gmail.com>
2015-09-22 13:24:08 +0300

    Scripts/Everbloom: DB snippet.

---
+++ src/server/scripts/Draenor/Everbloom/everbloom_version_1.1.sql.sql
@@ -0,0 +1,19 @@
+-- Creature Template
+
+UPDATE `ptr_world`.`creature_template` SET `dmg_multiplier`=38 WHERE  `entry`=819834;
+UPDATE `ptr_world`.`creature_template` SET `dmg_multiplier`=25 WHERE  `entry`=819833;
+UPDATE `ptr_world`.`creature_template` SET `dmg_multiplier`=30 WHERE  `entry`=819834;
+UPDATE `ptr_world`.`creature_template` SET `dmg_multiplier`=18 WHERE  `entry`=81983;
+UPDATE `ptr_world`.`creature_template` SET `dmg_multiplier`=38 WHERE  `entry`=819845;
+UPDATE `ptr_world`.`creature_template` SET `dmg_multiplier`=28 WHERE  `entry`=819844;
+UPDATE `ptr_world`.`creature_template` SET `dmg_multiplier`=20 WHERE  `entry`=81984;
+UPDATE `ptr_world`.`creature_template` SET `dmg_multiplier`=18 WHERE  `entry`=81984;
+UPDATE `ptr_world`.`creature_template` SET `dmg_multiplier`=16 WHERE  `entry`=83894;
+UPDATE `ptr_world`.`creature_template` SET `dmg_multiplier`=24 WHERE  `entry`=838945;
+UPDATE `ptr_world`.`creature_template` SET `dmg_multiplier`=30 WHERE  `entry`=838944;
+UPDATE `ptr_world`.`creature_template` SET `modelid1`=11686 WHERE  `entry`=88862;
+
+
+-- Spell Script Names
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (169179, 'the_everbloom_yalnu_cone_strike');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (168105, 'spell_everbloom_rapid_tides');
