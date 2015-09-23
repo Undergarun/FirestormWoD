e2d0e02cf95c018cecce65778ef2ac7e07c5e11e
Tuxity <kevin.darcel@gmail.com>
2015-09-22 19:56:25 -0500

    Project/SQL: Fix typo sql

--- sql/updates/world/2015_09_22_creature_template.sql
+++ sql/updates/world/2015_09_22_creature_template.sql
@@ -2,5 +2,5 @@
 UPDATE `creature_template` SET `ScriptName` = '' WHERE `entry` = 416;
 -- Remove Searing totem script
 UPDATE `creature_template` SET `ScriptName` = '' WHERE `ScriptName` = 'spell_npc_searing_totem';
---- Rename Frozenorb script
+-- Rename Frozen Orb script
 UPDATE `creature_template` SET `ScriptName` = 'spell_npc_mage_frozen_orb' WHERE `ScriptName` = 'npc_frozen_orb';
