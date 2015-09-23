21e32c025ecf32ea41f0e2c9868f8248e6cd3298
Tuxity <kevin.darcel@gmail.com>
2015-09-23 09:40:24 -0500

    SQL/Creature: Chromehound was summonable regardless the spec because it should be exotic

--- sql/updates/world/2015_09_23_creature_template.sql
+++ sql/updates/world/2015_09_23_creature_template.sql
@@ -5,3 +5,6 @@ UPDATE `creature_template` SET `type_flags` = 0x1 WHERE `entry` IN (76914, 80782
 UPDATE `creature_template` SET `type_flags` = 0x1 WHERE `entry` IN (82308, 77140, 84919, 82354, 79020);
   -- Some are exotic
 UPDATE `creature_template` SET `type_flags` = 0x1 + 0x10000 WHERE `entry` IN (88708, 88710);
+
+-- Chromehound
+UPDATE `creature_template` SET `type_flags` = 0x1 + 0x10000 WHERE `entry` = 50839;
