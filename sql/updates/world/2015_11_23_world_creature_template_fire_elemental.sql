8ac25c03885398e5d9b9b1dedac57d311af7d92d
pinkmouse <justin.mariette@gmail.com>
2015-11-23 10:18:50 +0100

    Spells/Shaman: Fix sql of Fire Elemental

---
+++ sql/updates/world/2015_11_23_world_creature_template_fire_elemental.sql
@@ -0,0 +1,13 @@
+UPDATE creature_template
+SET modelid1=1204,
+  spell1 = 57984,
+  spell2=117588,
+  spell3=13376,
+  spell4=0,
+  spell5=0,
+  spell6=0,
+  spell7=0,
+  spell8=0,
+  ScriptName = 'spell_npc_sha_fire_elemental',
+  BuildVerified=19865
+WHERE entry = 15438;
