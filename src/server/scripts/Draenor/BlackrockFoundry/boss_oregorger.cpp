e8033e68dace50cf13d4147e96f0d9178355a01c
Morgoporc <j.fraisse45@laposte.net>
2015-12-01 18:43:29 +0100

    Scripts/Foundry: Add last fixes before release - Fix Living Flames template - Reduce the radius for Cave In target selection - Reduce the effective radius of Acidback Puddle - Remove Acidback Puddle DoT at AreaTrigger removal - Add soft enrage mechanic for Oregorger (after 7 minutes) - Fix Gronnling Smash in a generic way
    Signed-off-by: Morgoporc <j.fraisse45@laposte.net>

--- src/server/scripts/Draenor/BlackrockFoundry/boss_oregorger.cpp
+++ src/server/scripts/Draenor/BlackrockFoundry/boss_oregorger.cpp
@@ -129,6 +129,7 @@ class boss_oregorger : public CreatureScript
             RollingFuryVisual           = 174183,
             RollingBox                  = 160665,
             WallshakingRoar             = 160662,
+            EarthshakingStomp           = 159958,
             /// Phase 1
             /// Acid Maw
             AcidMawDoT                  = 173471,
@@ -174,7 +175,8 @@ class boss_oregorger : public CreatureScript
         {
             EventCheckTrashs = 1,
             EventCollectOre,
-            EventCheckLastCollision
+            EventCheckLastCollision,
+            EventSoftEnrage
         };
 
         enum eCreatures
@@ -937,6 +939,20 @@ class boss_oregorger : public CreatureScript
                         m_CosmeticEvents.ScheduleEvent(eCosmeticEvents::EventCheckLastCollision, 1 * TimeConstants::IN_MILLISECONDS);
                         break;
                     }
+                    case eCosmeticEvents::EventSoftEnrage:
+                    {
+                        m_Phase = ePhases::PhaseFight;
+
+                        me->RemoveAura(eSpells::HungerDrivePeriodic);
+                        me->RemoveAura(eSpells::RollingFuryAura);
+
+                        m_Events.Reset();
+                        m_CosmeticEvents.Reset();
+
+                        me->CastSpell(me, eSpells::EarthshakingStomp, false);
+                        m_CosmeticEvents.ScheduleEvent(eCosmeticEvents::EventSoftEnrage, 1 * TimeConstants::IN_MILLISECONDS + 500);
+                        break;
+                    }
                     default:
                         break;
                 }
@@ -1037,6 +1053,8 @@ class boss_oregorger : public CreatureScript
 
                 if (me->HasAura(eSpells::BlackrockSpines))
                     m_Events.ScheduleEvent(eEvents::EventBlackrockBarrage, 100);
+
+                m_CosmeticEvents.RescheduleEvent(eCosmeticEvents::EventSoftEnrage, 7 * TimeConstants::MINUTE * TimeConstants::IN_MILLISECONDS);
             }
 
             uint8 GetCollisionOrIntersectionPoint(uint8 p_I, uint8 p_J) const
