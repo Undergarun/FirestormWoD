ac17dfc1eb5b2d45e8bcbadcbb9ea6fd096dac0f
Morgoporc <j.fraisse45@laposte.net>
2015-12-03 09:54:29 +0100

    Core/Units: Fix exhit vehicle moving when passenger died
    Signed-off-by: Morgoporc <j.fraisse45@laposte.net>

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -14685,7 +14685,9 @@ void Unit::setDeathState(DeathState s)
         if (IsNonMeleeSpellCasted(false))
             InterruptNonMeleeSpells(false);
 
+        StopMoving();
         ExitVehicle();
+
         UnsummonAllTotems();
         RemoveAllControlled();
         RemoveAllAurasOnDeath();
@@ -14708,7 +14710,7 @@ void Unit::setDeathState(DeathState s)
             GetMotionMaster()->Clear(false);
             GetMotionMaster()->MoveIdle();
         }
-        StopMoving();
+
         DisableSpline();
         // without this when removing IncreaseMaxHealth aura player may stuck with 1 hp
         // do not why since in IncreaseMaxHealth currenthealth is checked
