93badcecde08bb3c2015c5d2ca3c7ccc67881778
François B <francois@millenium-studio.fr>
2015-12-29 09:48:17 +0100

    Revert "Level condition for Mastery: Molten Earth."
    This reverts commit 02f73d844747981d4599506d650e0fcf758832dd.

--- src/server/scripts/Spells/spell_mastery.cpp
+++ src/server/scripts/Spells/spell_mastery.cpp
@@ -76,10 +76,6 @@ class spell_mastery_molten_earth : public SpellScriptLoader
                 if (l_Caster == nullptr || l_Target == nullptr)
                     return;
 
-                /// Mastery works from 80 lvl
-                if (l_Caster->getLevel() < 80)
-                    return;
-
                 /// Assuming it's a 33.33% proc chance
                 if (!roll_chance_f(33.33f))
                     return;
