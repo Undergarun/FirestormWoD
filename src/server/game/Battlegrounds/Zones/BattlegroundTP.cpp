9d1b8bfb5ab10b0d60d03ea72d3c82e5086af7eb
Morgoporc <j.fraisse45@laposte.net>
2016-01-08 10:55:13 +0100

    Core/Battlegrounds: Fix Kot'mogu and Twin Peaks RemovePlayer handler
    Signed-off-by: Morgoporc <j.fraisse45@laposte.net>

--- src/server/game/Battlegrounds/Zones/BattlegroundTP.cpp
+++ src/server/game/Battlegrounds/Zones/BattlegroundTP.cpp
@@ -631,7 +631,7 @@ void BattlegroundTP::EventPlayerClickedOnFlag(Player *Source, GameObject* target
     Source->RemoveAurasWithInterruptFlags(AURA_INTERRUPT_FLAG_ENTER_PVP_COMBAT);
 }
 
-void BattlegroundTP::RemovePlayer(Player *player, uint64 guid)
+void BattlegroundTP::RemovePlayer(Player *player, uint64 guid, uint32 /*team*/)
 {
     // sometimes flag auras are not removed :(
     if (IsAllianceFlagPickedup() && _flagKeepers[TEAM_ALLIANCE] == guid)
