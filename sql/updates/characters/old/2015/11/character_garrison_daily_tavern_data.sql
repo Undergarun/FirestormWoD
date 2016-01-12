283438f9830494b2a11ed102683a25fc0b12a851
Jeyel <contact.jl@voila.fr>
2016-01-12 17:22:38 +0100

    Database/Garrison : Modify primary key for Tavern table (character_garrison_daily_tavern_data)

--- sql/updates/characters/old/2015/11/character_garrison_daily_tavern_data.sql
+++ sql/updates/characters/old/2015/11/character_garrison_daily_tavern_data.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS character_garrison_daily_tavern_data;
 CREATE TABLE `character_garrison_daily_tavern_data` (
   `CharacterGuid` int(10) unsigned NOT NULL DEFAULT '0',
-  `NpcEntry` int(8) NOT NULL
+  `NpcEntry` int(8) NOT NULL,
+  PRIMARY KEY (`CharacterGuid`, `NpcEntry`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
