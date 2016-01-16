06383f45763ec99583efcce2654dbca09f7a232f
twink <nikazhazha@mail.ru>
2016-01-16 05:23:48 +0200

    Fix Mastery: Unshackled Fury after pinkmouse changes (https://github.com/MilleniumStudio/Garrosh/commit/bb04e2ccc0c8a3c4d002d7da370e35bc7387fb8e#diff-4522eeffcbc961602e86620011a2d579R10634)
    - it was working all the time, and when warrior receive enrage his mastery was working twice

--- src/server/game/Entities/Unit/StatSystem.cpp
+++ src/server/game/Entities/Unit/StatSystem.cpp
@@ -893,7 +893,8 @@ void Player::UpdateMasteryPercentage()
                 if (AuraEffectPtr l_AurEff = l_Aura->GetEffect(l_I))
                 {
                     l_AurEff->SetCanBeRecalculated(true);
-                    if ((l_SpellInfo->Id == 77219 && !HasAura(103958) && l_I >= EFFECT_2)) ///< EFFECT_2 and EFFECT_3 of Master Demonologist are only on Metamorphis Form
+                    if ((l_SpellInfo->Id == 77219 && !HasAura(103958) && l_I >= EFFECT_2) ///< EFFECT_2 and EFFECT_3 of Master Demonologist are only on Metamorphis Form
+                        || (l_SpellInfo->Id == 76856 && !HasAura(12880))) ///< Mastery : Unshackled Fury
                         l_AurEff->ChangeAmount(0, true, true);
                     else
                     {
