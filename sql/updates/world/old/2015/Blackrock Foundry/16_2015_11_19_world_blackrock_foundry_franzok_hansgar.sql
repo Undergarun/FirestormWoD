1894cea25cf27a01d8992336c595bb68370838b3
Morgoporc <j.fraisse45@laposte.net>
2016-01-20 15:27:39 +0100

    Database/Foundry: Add loots for Hans'gar & Franzok

--- sql/updates/world/old/2015/Blackrock Foundry/16_2015_11_19_world_blackrock_foundry_franzok_hansgar.sql
+++ sql/updates/world/old/2015/Blackrock Foundry/16_2015_11_19_world_blackrock_foundry_franzok_hansgar.sql
@@ -59,42 +59,42 @@ DELETE FROM locales_creature_text WHERE entry IN (@REF_HANSGAR, @REF_FRANZOK);
 INSERT INTO locales_creature_text (entry, textGroup, id, text_loc2, text_loc3, text_loc6, text_loc8) VALUES
 (
     @REF_HANSGAR, 0, 0,
-    'Dis donc, Franz... Franzok ! La machine est coincée !',
+    'Dis donc, Franz... Franzok ! La machine est coincée !',
     'Hey Franz? Franzok!! Das Ding steckt fest!',
     'Ey, Franz. ¡¡Franzok!! ¡Esto está atascado!',
     'Франц? Франзок! Конвейер встал!'
 ),
 (
     @REF_HANSGAR, 1, 0,
-    'Jamais de la vie ! Je l''ai déjà décoincée la dernière fois !',
+    'Jamais de la vie ! Je l''ai déjà décoincée la dernière fois !',
     'Ich reparier''s nich''! Ich war das letzte Mal dran!',
     '¡No voy a desatascarlo! ¡Ya lo desatasqué la última vez!',
     'Я уже в прошлый раз все чинил! Хватит!'
 ),
 (
     @REF_HANSGAR, 2, 0,
-    'Hé, vous ! Rentrez là-dedans et décoincez-moi cette presse !',
+    'Hé, vous ! Rentrez là-dedans et décoincez-moi cette presse !',
     'Ey, du! Spuck in die Hände und reparier den Stanzer!',
     '¡Oye, tú! ¡Ven y desatasca la estampadora!',
     'Эй, вы! Быстро сюда, почините эту штуку!'
 ),
 (
     @REF_HANSGAR, 3, 0,
-    'Je vais vous ÉCRASER !',
+    'Je vais vous ÉCRASER !',
     'Ich mach Euch PLATT!',
     '¡Voy a ELIMINARTE!',
     'Ну все, вам крышка!'
 ),
 (
     @REF_HANSGAR, 4, 0,
-    'Arrêtez de vous cogner !',
+    'Arrêtez de vous cogner !',
     'Hört auf, Euch selbst zu hau''n!',
     '¡Dejen de pegarse a sí mismos!',
     'Хватит лупить себя!'
 ),
 (
     @REF_HANSGAR, 5, 0,
-    'Je reviendrai !',
+    'Je reviendrai !',
     'Ich komme wieder!',
     '¡Volveré!',
     'Айл би бэк!'
@@ -108,7 +108,7 @@ INSERT INTO locales_creature_text (entry, textGroup, id, text_loc2, text_loc3, t
 ),
 (
     @REF_HANSGAR, 7, 0,
-    'Aarrgh ! Javais pas encore fini...',
+    'Aarrgh ! Javais pas encore fini...',
     'Uups! Ich war noch nich'' fertig...',
     'Aún no había terminado...',
     'Гр-р-р! Я не закончил...'
@@ -122,28 +122,28 @@ INSERT INTO locales_creature_text (entry, textGroup, id, text_loc2, text_loc3, t
 ),
 (
     @REF_HANSGAR, 9, 0,
-    '|TInterface\\Icons\\ability_hanzandfranz_bodyslam.blp:20|t$n est sur le point de se faire toucher par un |cFFF00000|Hspell:159646|h[Plaquage]|h|r !',
+    '|TInterface\\Icons\\ability_hanzandfranz_bodyslam.blp:20|t$n est sur le point de se faire toucher par un |cFFF00000|Hspell:159646|h[Plaquage]|h|r !',
     '|TInterface\\Icons\\ability_hanzandfranz_bodyslam.blp:20|t$n ist Ziel von |cFFF00000|Hspell:159646|h[Bodycheck]|h|r!',
     '|TInterface\\Icons\\ability_hanzandfranz_bodyslam.blp:20|t¡$n va a recibir un |cFFF00000|Hspell:159646|h[Embate corporal]|h|r!',
     '|TInterface\\Icons\\ability_hanzandfranz_bodyslam.blp:20|t$n сейчас получит урон от способности |cFFF00000|Hspell:159646|h[Мощный удар]|h|r!'
 ),
 (
     @REF_FRANZOK, 0, 0,
-    'Et alors ? T''as qu''à la décoincer !',
+    'Et alors ? T''as qu''à la décoincer !',
     'Und? Geh''s halt reparieren.',
     '¿Y? ¡Desatáscalo, entonces!',
     'Ну так почини его!'
 ),
 (
     @REF_FRANZOK, 1, 0,
-    'Tu veux aller dire à Main-Noire qu''on a un laminoir qui lamine pas ?',
+    'Tu veux aller dire à Main-Noire qu''on a un laminoir qui lamine pas ?',
     'Willst du Schwarzfaust sagen gehen, dass wir einen Stanzer haben, der nich'' stanzt?',
     '¿Quieres decirle a Puño Negro que tenemos una estampadora que no estampa?',
     'Рискнешь сказать Черноруку, что нам штамповать нечем?'
 ),
 (
     @REF_FRANZOK, 2, 0,
-    'Ha ha ha ! Il y aura des impuretés dans ce lot.',
+    'Ha ha ha ! Il y aura des impuretés dans ce lot.',
     'Ha ha ha! Diese Fuhre wird wohl ein paar Unreinheiten haben.',
     '¡Ja ja ja! Creo que este lote tiene algunas impurezas.',
     'Ха-ха-ха! Эта партия выйдет с дефектами.'
@@ -157,28 +157,28 @@ INSERT INTO locales_creature_text (entry, textGroup, id, text_loc2, text_loc3, t
 ),
 (
     @REF_FRANZOK, 4, 0,
-    'Faites chauffer la forge !',
+    'Faites chauffer la forge !',
     'Wir pumpen den Ofen in den Turbogang!',
     '¡Eleva la potencia de la forja para conseguir la Ultravelocidad!',
     'Включаем режим перегрева!'
 ),
 (
     @REF_FRANZOK, 5, 0,
-    'Ha, ça vous a plu ? Attendez pour voir !!!',
+    'Ha, ça vous a plu ? Attendez pour voir !!!',
     'Heh, das hat Euch beeindruckt?! Wartet''s nur ab!!',
     'Je, ¡¿piensas que eso estuvo bueno?! ¡Ya verás!',
     'И это, по-твоему, круто? Ну погоди!'
 ),
 (
     @REF_FRANZOK, 6, 0,
-    'Si ça saigne, c''est qu''on peut le tuer !',
+    'Si ça saigne, c''est qu''on peut le tuer !',
     'Wenn es blutet, können wir es töten!',
     'Si sangra, ¡podemos matarlo!',
     'Течет кровь - можно убить!'
 ),
 (
     @REF_FRANZOK, 7, 0,
-    'Il ne peut en rester que deux, ha ha ha !',
+    'Il ne peut en rester que deux, ha ha ha !',
     'Es kann nur zwei geben, ah hahaha!',
     '¡Solo puede haber dos, jajaja!',
     'Останутся лишь двое! Ха-ха-ха!'
@@ -192,18 +192,40 @@ INSERT INTO locales_creature_text (entry, textGroup, id, text_loc2, text_loc3, t
 ),
 (
     @REF_FRANZOK, 9, 0,
-    '|TInterface\\Icons\\ability_garrosh_hellscreams_warsong.blp:20|t%s commence à lancer |cFFF00000|Hspell:160838|h[Rugissement perturbant]|h|r !',
+    '|TInterface\\Icons\\ability_garrosh_hellscreams_warsong.blp:20|t%s commence à lancer |cFFF00000|Hspell:160838|h[Rugissement perturbant]|h|r !',
     '|TInterface\\Icons\\ability_garrosh_hellscreams_warsong.blp:20|t%s fängt an, |cFFF00000|Hspell:160838|h[Störendes Brüllen]|h|r zu wirken!',
     '|TInterface\\Icons\\ability_garrosh_hellscreams_warsong.blp:20|t¡%s empieza a lanzar |cFFF00000|Hspell:160838|h[Rugido perturbador]|h|r!',
     '|TInterface\\Icons\\ability_garrosh_hellscreams_warsong.blp:20|t%s начинает произносить заклинание |cFFF00000|Hspell:160838|h[Разрушительный рев]|h|r!'
 ),
 (
     @REF_FRANZOK, 10, 0,
-    '|TInterface\\Icons\\ability_hanzandfranz_bodyslam.blp:20|t$n est sur le point de se faire toucher par un |cFFF00000|Hspell:159646|h[Plaquage]|h|r !',
+    '|TInterface\\Icons\\ability_hanzandfranz_bodyslam.blp:20|t$n est sur le point de se faire toucher par un |cFFF00000|Hspell:159646|h[Plaquage]|h|r !',
     '|TInterface\\Icons\\ability_hanzandfranz_bodyslam.blp:20|t$n ist Ziel von |cFFF00000|Hspell:159646|h[Bodycheck]|h|r!',
     '|TInterface\\Icons\\ability_hanzandfranz_bodyslam.blp:20|t¡$n va a recibir un |cFFF00000|Hspell:159646|h[Embate corporal]|h|r!',
     '|TInterface\\Icons\\ability_hanzandfranz_bodyslam.blp:20|t$n сейчас получит урон от способности |cFFF00000|Hspell:159646|h[Мощный удар]|h|r!'
 );
+
+DELETE FROM `reference_loot_template` WHERE entry = @REF_HANSGAR;
+INSERT INTO `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(@REF_HANSGAR, 113897, 0, 1, 1, 1, 1),
+(@REF_HANSGAR, 113898, 0, 1, 1, 1, 1),
+(@REF_HANSGAR, 113899, 0, 1, 1, 1, 1),
+(@REF_HANSGAR, 113900, 0, 1, 1, 1, 1),
+(@REF_HANSGAR, 113901, 0, 1, 1, 1, 1),
+(@REF_HANSGAR, 113902, 0, 1, 1, 1, 1),
+(@REF_HANSGAR, 113903, 0, 1, 1, 1, 1),
+(@REF_HANSGAR, 113904, 0, 1, 1, 1, 1),
+(@REF_HANSGAR, 113905, 0, 1, 1, 1, 1),
+(@REF_HANSGAR, 113906, 0, 1, 1, 1, 1),
+(@REF_HANSGAR, 113907, 0, 1, 1, 1, 1),
+(@REF_HANSGAR, 113908, 0, 1, 1, 1, 1),
+(@REF_HANSGAR, 113910, 0, 1, 1, 1, 1);
+
+UPDATE `creature_template` SET `lootid`= @REF_HANSGAR WHERE `entry`= @REF_HANSGAR;
+
+DELETE FROM `creature_loot_template` WHERE `entry`= @REF_HANSGAR;
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(@REF_HANSGAR, 1, 100, 1, 0, -@REF_HANSGAR, 6);
 
 DELETE FROM creature_groupsizestats WHERE entry = @REF_HANSGAR;
 INSERT INTO creature_groupsizestats (entry, difficulty, groupSize, health) VALUES
