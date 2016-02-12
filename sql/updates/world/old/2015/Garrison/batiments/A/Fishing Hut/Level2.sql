146d50c7d2e228395fdc04a0eced7684e0264289
Jeyel <contact.jl@voila.fr>
2016-02-12 18:04:48 +0100

    Database/SQL : Refactor the whole Garrison building SQL files + add fixes

--- sql/updates/world/old/2015/Garrison/batiments/A/Fishing Hut/Level2.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM garrison_plot_content WHERE plot_type_or_building=-134 AND faction_index = 1;
-insert into `garrison_plot_content` (`plot_type_or_building`, `faction_index`, `creature_or_gob`, `x`, `y`, `z`, `o`) values('-134','1','85708','13.089','-0.749152','0.500114','6.03866');
-insert into `garrison_plot_content` (`plot_type_or_building`, `faction_index`, `creature_or_gob`, `x`, `y`, `z`, `o`) values('-134','1','82436','7.06115','9.60846','0.500175','5.94363');
-insert into `garrison_plot_content` (`plot_type_or_building`, `faction_index`, `creature_or_gob`, `x`, `y`, `z`, `o`) values('-134','1','77733','11.8191','-11.1024','0.292328','4.89748');
-insert into `garrison_plot_content` (`plot_type_or_building`, `faction_index`, `creature_or_gob`, `x`, `y`, `z`, `o`) values('-134','1','-232268','8.2991','1.22775','-0.000221','0.880167');
--- sql/updates/world/old/2015/Garrison/batiments/H/Fishing Hut/Level3.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM garrison_plot_content WHERE plot_type_or_building = -135 AND faction_index = 0;
-insert into `garrison_plot_content` (`plot_type_or_building`, `faction_index`, `creature_or_gob`, `x`, `y`, `z`, `o`) values('-135','0','79971','1.62224','9.4448','0.949738','5.90643');
-insert into `garrison_plot_content` (`plot_type_or_building`, `faction_index`, `creature_or_gob`, `x`, `y`, `z`, `o`) values('-135','0','79892','0.171587','0.551334','0.503006','5.2035');
-insert into `garrison_plot_content` (`plot_type_or_building`, `faction_index`, `creature_or_gob`, `x`, `y`, `z`, `o`) values('-135','0','-232440','9.48394','1.06858','-0.000076','3.22037');
-insert into `garrison_plot_content` (`plot_type_or_building`, `faction_index`, `creature_or_gob`, `x`, `y`, `z`, `o`) values('-135','0','-230764','3.94956','7.22322','0.107117','3.22822');
