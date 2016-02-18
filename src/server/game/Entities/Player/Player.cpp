db6ecd32e53606eeb29232b3a50a1301abcbbbfb
Izidor <millenium_woweur@hotmail.fr>
2016-02-18 11:34:46 +0100

    Cross/Battleground: Fix log.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -20731,7 +20731,7 @@ void Player::_LoadBGData(PreparedQueryResult result)
     m_bgData.mountSpell   = fields[9].GetUInt32();
     m_bgData.m_LastActiveSpec = fields[10].GetUInt16();
 
-    sLog->outAshran("Player::_LoadBGData guid : %u bgInstanceID : ", GetGUIDLow(), m_bgData.bgInstanceID);
+    sLog->outAshran("Player::_LoadBGData guid : %u bgInstanceID : %u", GetGUIDLow(), m_bgData.bgInstanceID);
 }
 
 bool Player::LoadPositionFromDB(uint32& mapid, float& x, float& y, float& z, float& o, bool& in_flight, uint64 guid)
