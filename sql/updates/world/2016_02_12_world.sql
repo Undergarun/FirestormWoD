0b3f04fd376e82a8c3facde66b946acfbaab2ff7
François B <francois@millenium-studio.fr>
2016-02-19 10:44:33 +0100

    Database: Update base, the travis build broken since one week !

---
+++ sql/updates/world/2016_02_12_world.sql
@@ -0,0 +1,5 @@
+-- old or duplicate table
+DROP TABLE `gameobject_origin_copy`;
+DROP TABLE `creature_template_mop`;
+DROP TABLE `creature_template_difficulty_backup`;
+DROP TABLE `creature_template_difficulty_bab`;
