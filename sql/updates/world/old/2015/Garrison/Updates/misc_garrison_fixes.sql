d6b4d7d8f1f13785d0fa84c7f55dc8a6e807293f
Jeyel <contact.jl@voila.fr>
2016-02-24 10:17:52 +0100

    Database/Garrison : Fix a few things

--- sql/updates/world/old/2015/Garrison/Updates/misc_garrison_fixes.sql
+++ sql/updates/world/old/2015/Garrison/Updates/misc_garrison_fixes.sql
@@ -137,7 +137,7 @@
 
 #35176/Quest - Keeping it Together
 
-	insert into `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) values('84455','0','0','0','64','0','100','0','0','0','0','0','33','84455','0','0','0','0','0','0','0','0','0','0','0','0','0','Assistant Brightstone kill credit');
+	replace into `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) values('84455','0','0','0','64','0','100','0','0','0','0','0','33','84455','0','0','0','0','0','0','0','0','0','0','0','0','0','Assistant Brightstone kill credit');
 
 #33787/Quest - Fiona s solution
 
