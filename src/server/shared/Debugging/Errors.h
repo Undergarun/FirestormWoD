1182e4ab5699761a0bc386c3ba65bd85fbab9f56
Izidor <millenium_woweur@hotmail.fr>
2016-02-26 18:44:59 +0100

    Revert "Core/Shared: Remove assert (should improve performance)."
    This reverts commit f9abd94238ccec08892c17bb3807c4caa2850e4b.

--- src/server/shared/Debugging/Errors.h
+++ src/server/shared/Debugging/Errors.h
@@ -24,7 +24,7 @@
 #include <ace/Stack_Trace.h>
 #include <ace/OS_NS_unistd.h>
 
-#define WPAssert(assertion) { /*if (!(assertion)) { sLog->outAshran("\n%s:%i in %s ASSERTION FAILED:\n  %s\n%", __FILE__, __LINE__, __FUNCTION__, #assertion); }*/ }
+#define WPAssert(assertion) { if (!(assertion)) { sLog->outAshran("\n%s:%i in %s ASSERTION FAILED:\n  %s\n%", __FILE__, __LINE__, __FUNCTION__, #assertion); } }
 #define WPError(assertion, errmsg) { if (!(assertion)) { sLog->outError(LOG_FILTER_GENERAL, "%\n%s:%i in %s ERROR:\n  %s\n", __FILE__, __LINE__, __FUNCTION__, (char *)errmsg); } }
 #define WPWarning(assertion, errmsg) { if (!(assertion)) { sLog->outError(LOG_FILTER_GENERAL, "\n%s:%i in %s WARNING:\n  %s\n", __FILE__, __LINE__, __FUNCTION__, (char *)errmsg); } }
 #define WPFatal(assertion, errmsg) { if (!(assertion)) { sLog->outError(LOG_FILTER_GENERAL, "\n%s:%i in %s FATAL ERROR:\n  %s\n", __FILE__, __LINE__, __FUNCTION__, (char *)errmsg); ACE_OS::sleep(10); } }
