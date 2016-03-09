09cd2f2674e98e681cd4422d447944dc826f42da
Morgoporc <j.fraisse45@laposte.net>
2016-03-09 11:02:55 +0100

    Scripts/Highmaul: Try to fix Vul'gor's door issue
    Signed-off-by: Morgoporc <j.fraisse45@laposte.net>

--- src/server/scripts/Draenor/Highmaul/boss_kargath_bladefist.cpp
+++ src/server/scripts/Draenor/Highmaul/boss_kargath_bladefist.cpp
@@ -1028,7 +1028,7 @@ class npc_highmaul_vulgor : public CreatureScript
                         uint8 l_Count = 0;
                         for (Creature* l_Sorcerer : l_BladespireSorcerers)
                         {
-                            l_Sorcerer->SetWalk(true);
+                            l_Sorcerer->SetWalk(false);
                             l_Sorcerer->GetMotionMaster()->MovePoint(eMove::MoveInArena, g_SorcererPos[l_Count]);
                             m_SorcererGuids[l_Count] = l_Sorcerer->GetGUID();
 
@@ -1036,7 +1036,7 @@ class npc_highmaul_vulgor : public CreatureScript
                         }
                     }
 
-                    me->SetWalk(true);
+                    me->SetWalk(false);
                     me->GetMotionMaster()->MovePoint(eMove::MoveInArena, g_VulgorMovePos);
                 }
             }
@@ -1091,6 +1091,21 @@ class npc_highmaul_vulgor : public CreatureScript
                         if (Creature* l_Sorcerer = me->SummonCreature(eHighmaulCreatures::BladespireSorcerer, g_TrashsSpawnPos))
                             l_Sorcerer->GetMotionMaster()->MovePoint(eMove::MoveInArena, g_SorcererSecondPos[l_I]);
                     }
+
+                    if (m_Instance != nullptr)
+                    {
+                        if (GameObject* l_InnerGate = GameObject::GetGameObject(*me, m_Instance->GetData64(eHighmaulGameobjects::GateArenaInner)))
+                            l_InnerGate->SetGoState(GOState::GO_STATE_ACTIVE);
+                    }
+
+                    AddTimedDelayedOperation(5 * TimeConstants::IN_MILLISECONDS, [this]() -> void
+                    {
+                        if (m_Instance != nullptr)
+                        {
+                            if (GameObject* l_InnerGate = GameObject::GetGameObject(*me, m_Instance->GetData64(eHighmaulGameobjects::GateArenaInner)))
+                                l_InnerGate->SetGoState(GOState::GO_STATE_READY);
+                        }
+                    });
                 }
             }
 
