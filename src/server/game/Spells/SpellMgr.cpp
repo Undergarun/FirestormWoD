12f91ae54c9b5bc8b983cf19cfd610e460dc6262
Morgoporc <j.fraisse45@laposte.net>
2016-03-15 10:23:08 +0100

    Core/Spells: Fists of Stone damages are split between targets
    Signed-off-by: Morgoporc <j.fraisse45@laposte.net>

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3609,6 +3609,7 @@ void SpellMgr::LoadSpellCustomAttr()
                 spellInfo->AttributesCu |= SPELL_ATTR0_CU_NEGATIVE;
                 break;
             case 154938: ///< Molten Torrent (AoE Damage - 154938)
+            case 162349: ///< Fists of Stone (Kromog)
                 spellInfo->AttributesCu |= SPELL_ATTR0_CU_SHARE_DAMAGE;
                 break;
             case 155745: ///< Charring Breath (Jump - Overheated Cinderwolf)
