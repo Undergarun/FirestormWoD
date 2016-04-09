65244538aab3f00d8fbdbd9674ea288b5b6f7403
Daniel Edri <dobidoberman1@gmail.com>
2016-04-09 12:40:15 +0300

    Spells/Spellmgr.cpp:  Shadowmoon Burial Grounds
    How did I forgot to push the spellmgr.cpp?

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3806,63 +3806,64 @@ void SpellMgr::LoadSpellCustomAttr()
                 spellInfo->ProcFlags = 0;
                 spellInfo->ProcChance = 0;
                 break;
-                /// Shadowmoon Burial Grounds
-            case 153068: ///< Void Devestation
-                spellInfo->Effects[0].TargetA = TARGET_DEST_CASTER;
-                spellInfo->Effects[0].TargetB = TARGET_DEST_DEST_RADIUS;
-                spellInfo->Effects[0].RadiusEntry = sSpellRadiusStore.LookupEntry(10); // 30y
-                break;
-            case 152962: ///< Soul Steal
-                spellInfo->Effects[0].TriggerSpell = 0;
-                spellInfo->Effects[0].TargetA = TARGET_UNIT_TARGET_ANY;
-                spellInfo->Effects[0].TargetB = NULL;
-                break;
-            case 152979: ///< Soul Shread
-                spellInfo->Effects[0].TargetA = TARGET_UNIT_TARGET_ANY;
-                spellInfo->Effects[1].TargetA = TARGET_UNIT_TARGET_ANY;
-                break;
-            case 164693: ///< Lunar Runes
-            case 164695:
-            case 164696:
-                spellInfo->DurationEntry = sSpellDurationStore.LookupEntry(4); ///< 120s
-                break;
-            case 154327: ///< Domination
-                spellInfo->Effects[0].TargetA = TARGET_UNIT_TARGET_ANY;
-                break;
-            case 153164: ///< Dark Communion
-                spellInfo->Effects[0].TargetA = TARGET_UNIT_CASTER;
-                spellInfo->Effects[1].TargetA = TARGET_UNIT_CASTER;
-                spellInfo->Effects[2].TargetA = TARGET_UNIT_CASTER;
-                spellInfo->Effects[0].TargetB = NULL;
-                spellInfo->Effects[1].TargetB = NULL;
-                spellInfo->Effects[2].TargetB = NULL;
-                break;
-            case 153153: ///< Dark Communion
-                spellInfo->Effects[0].TargetA = TARGET_UNIT_TARGET_ANY;
-                spellInfo->Effects[0].TargetB = NULL;
-                break;
-            case 153501: ///< Void Blast
-            case 153070: ///< Void Devestation
-                spellInfo->DurationEntry = sSpellDurationStore.LookupEntry(39); // 2s
-                spellInfo->AttributesCu |= SPELL_ATTR0_CU_NEGATIVE;
-                break;
-            case 173073: ///< Ground Marker
-                sSpellDurationStore.LookupEntry(1); ///< 10s
-                break;
-            case 153686: ///< Body Slam
-                spellInfo->AttributesEx2 |= SPELL_ATTR2_CAN_TARGET_NOT_IN_LOS;
-                break;
+				/// Shadowmoon Burial Grounds
+			case 153068: ///< Void Devestation
+				spellInfo->Effects[0].TargetA = TARGET_DEST_CASTER;
+				spellInfo->Effects[0].TargetB = TARGET_DEST_DEST_RADIUS;
+				spellInfo->Effects[0].RadiusEntry = sSpellRadiusStore.LookupEntry(10); // 30y
+				break;
+			case 152962: ///< Soul Steal
+				spellInfo->Effects[0].TriggerSpell = 0;
+				spellInfo->Effects[0].TargetA = TARGET_UNIT_TARGET_ANY;
+				spellInfo->Effects[0].TargetB = NULL;
+				break;
+			case 152979: ///< Soul Shreads
+				spellInfo->Effects[0].TargetA = TARGET_UNIT_TARGET_ANY;
+				spellInfo->Effects[1].TargetA = TARGET_UNIT_TARGET_ANY;
+				spellInfo->AttributesCu |= SPELL_ATTR0_CU_NEGATIVE;
+				break;
+			case 164693: ///< Lunar Runes
+			case 164695:
+			case 164696:
+				spellInfo->DurationEntry = sSpellDurationStore.LookupEntry(4); ///< 120s
+				break;
+			case 154327: ///< Domination
+				spellInfo->Effects[0].TargetA = TARGET_UNIT_TARGET_ANY;
+				break;
+			case 153164: ///< Dark Communion
+				spellInfo->Effects[0].TargetA = TARGET_UNIT_CASTER;
+				spellInfo->Effects[1].TargetA = TARGET_UNIT_CASTER;
+				spellInfo->Effects[2].TargetA = TARGET_UNIT_CASTER;
+				spellInfo->Effects[0].TargetB = NULL;
+				spellInfo->Effects[1].TargetB = NULL;
+				spellInfo->Effects[2].TargetB = NULL;
+				break;
+			case 153153: ///< Dark Communion
+				spellInfo->Effects[0].TargetA = TARGET_UNIT_TARGET_ANY;
+				spellInfo->Effects[0].TargetB = NULL;
+				break;
+			case 153501: ///< Void Blast
+			case 153070: ///< Void Devestation
+				spellInfo->DurationEntry = sSpellDurationStore.LookupEntry(39); // 2s
+				spellInfo->AttributesCu |= SPELL_ATTR0_CU_NEGATIVE;
+				break;
+			case 173073: ///< Ground Marker
+				sSpellDurationStore.LookupEntry(1); ///< 10s
+				break;
+			case 153686: ///< Body Slam
+				spellInfo->AttributesEx2 |= SPELL_ATTR2_CAN_TARGET_NOT_IN_LOS;
+				break;
 			case 154469: ///< Ritaul Of Bones - Darkness
-            case 153692: ///< Necrotic Pitch Debuff
-                spellInfo->DurationEntry = sSpellDurationStore.LookupEntry(39); // 2s
-                break;
-            case 153236: ///< DaggerFall
-                spellInfo->Effects[0].TargetA = 0;
-                spellInfo->Effects[0].TargetB = 0;
-                break;
-            case 164685: ///< Dark Eclipse
-                spellInfo->Effects[0].Amplitude = 600;
-                break;
+			case 153692: ///< Necrotic Pitch Debuff
+				spellInfo->DurationEntry = sSpellDurationStore.LookupEntry(39); // 2s
+				break;
+			case 153236: ///< DaggerFall
+				spellInfo->Effects[0].TargetA = 0;
+				spellInfo->Effects[0].TargetB = 0;
+				break;
+			case 164685: ///< Dark Eclipse
+				spellInfo->Effects[0].Amplitude = 600;
+				break;
                 /// Everbloom
             case 164643: ///< Rending Charge
             case 164886: ///< Dreadpetal Toxin
@@ -3962,6 +3963,9 @@ void SpellMgr::LoadSpellCustomAttr()
                 spellInfo->Effects[0].TargetB = TARGET_DEST_DEST;
                 break;
                 /// Auchindoun
+			case 157505: ///< Arcane Bolt
+				spellInfo->Effects[0].Amplitude = 1500;
+				break;
             case 154340: ///< Sanctified Ground
                 spellInfo->DurationEntry = sSpellDurationStore.LookupEntry(36); // 1s
                 break;
