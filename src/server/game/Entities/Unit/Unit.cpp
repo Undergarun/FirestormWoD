7cf3c02e02bfcfe32ce5af9e9b846eeaa1cf5499
pinkmouse <justin.mariette@gmail.com>
2016-04-18 16:07:20 +0200

    Core/Spells: Fix bonus percent damage apply too times, like paladin Avening wrath

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -13415,7 +13415,8 @@ uint32 Unit::MeleeDamageBonusDone(Unit* victim, uint32 pdamage, WeaponAttackType
 
     if (spellProto)
     {
-        if (!(spellProto->AttributesEx6 & SPELL_ATTR6_NO_DONE_PCT_DAMAGE_MODS))
+        if (!(spellProto->AttributesEx6 & SPELL_ATTR6_NO_DONE_PCT_DAMAGE_MODS) && !spellProto->HasEffect(SPELL_EFFECT_WEAPON_DAMAGE) &&
+            !spellProto->HasEffect(SPELL_EFFECT_WEAPON_PERCENT_DAMAGE) && !spellProto->HasEffect(SPELL_EFFECT_WEAPON_DAMAGE_NOSCHOOL)) ///< Already apply on CalculateDamage by TOTAL_PCT
         {
             AuraEffectList const& mModDamagePercentDone = GetAuraEffectsByType(SPELL_AURA_MOD_DAMAGE_PERCENT_DONE);
             for (AuraEffectList::const_iterator i = mModDamagePercentDone.begin(); i != mModDamagePercentDone.end(); ++i)
