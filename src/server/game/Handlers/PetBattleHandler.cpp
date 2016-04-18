87436af6a94c6da048960284033c6a5916ab3ab0
merydwin <merydwin@gmail.com>
2016-04-18 16:45:46 +0200

    Core/PetBattle: Fix a mistake in my crash fix

--- src/server/game/Handlers/PetBattleHandler.cpp
+++ src/server/game/Handlers/PetBattleHandler.cpp
@@ -1140,7 +1140,7 @@ void WorldSession::HandlePetBattleRequestUpdate(WorldPacket& p_RecvData)
             ++l_PlayerOpposantPetCount;
         }
 
-        if (!l_PlayerOpposantPetCount || l_PlayerPetCount)
+        if (!l_PlayerOpposantPetCount || !l_PlayerPetCount)
         {
             m_Player->GetSession()->SendPetBattleRequestFailed(PETBATTLE_REQUEST_NO_PETS_IN_SLOT);
             l_Opposant->GetSession()->SendPetBattleRequestFailed(PETBATTLE_REQUEST_NO_PETS_IN_SLOT);
