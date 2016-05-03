45550be7a96314b3bcf7f183fd113e0464c836c6
François B <francois@millenium-studio.fr>
2016-05-03 19:16:40 +0200

    Revert "Revert "CMake: remove the ugly macro copy pasted everywhere""
    This reverts commit b03a6fe96766d24fa39ad27aac8cb206fef4f283.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -11,6 +11,6 @@ option(USE_SCRIPTPCH    "Use precompiled headers when compiling scripts"        
 option(USE_COREPCH      "Use precompiled headers when compiling server"               1)
 option(WITH_WARNINGS    "Show all warnings during compile"                            0)
 option(WITH_COREDEBUG   "Include additional debug-code in core"                       0)
-set(WITH_SOURCE_TREE "no" CACHE STRING "Build the source tree for IDE's.")
+set(WITH_SOURCE_TREE "hierarchical" CACHE STRING "Build the source tree for IDE's.")
 set_property(CACHE WITH_SOURCE_TREE PROPERTY STRINGS no flat hierarchical)
 option(WITHOUT_GIT      "Disable the GIT testing routines"                            0)
