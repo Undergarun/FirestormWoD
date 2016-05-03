46e0382f3622a4948846dea23f9b189d8be00177
François B <francois@millenium-studio.fr>
2016-05-03 19:11:58 +0200

    Revert "Cmake: Try to fix the load under MSVC"
    This reverts commit 91e03f5ca5b3bbdc1ff4d52f4c71a6910f62799e.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -12,5 +12,5 @@ option(USE_COREPCH      "Use precompiled headers when compiling server"         
 option(WITH_WARNINGS    "Show all warnings during compile"                            0)
 option(WITH_COREDEBUG   "Include additional debug-code in core"                       0)
 set(WITH_SOURCE_TREE "hierarchical" CACHE STRING "Build the source tree for IDE's.")
-set_property(CACHE WITH_SOURCE_TREE PROPERTY STRINGS no flat hierarchical hierarchical-folders)
+set_property(CACHE WITH_SOURCE_TREE PROPERTY STRINGS no flat hierarchical)
 option(WITHOUT_GIT      "Disable the GIT testing routines"                            0)
