f0573238cae5a63bb971d3e09c70f7da84f35df4
François B <francois@millenium-studio.fr>
2016-05-04 12:20:21 +0200

    Revert "CMake : start to port https://github.com/TrinityCore/TrinityCore/commit/1d2aafd39bcb79a67357d198ce9b2345642fdd39"
    This reverts commit 0b4d3f828b81187565db5217e87512943b3d6de1.

--- src/server/shared/PrecompiledHeaders/sharedPCH.h
+++ src/server/shared/PrecompiledHeaders/sharedPCH.h
@@ -1,10 +1,9 @@
+//add here most rarely modified headers to speed up debug build compilation
+
 #include "Common.h"
 #include "Log.h"
 #include "DatabaseWorker.h"
 #include "SQLOperation.h"
 #include "Errors.h"
 #include "TypeList.h"
-#include "TaskScheduler.h"
-#include "EventMap.h"
-#include <ace/Basic_Types.h>
-#include <ace/ACE_export.h>
+#include "GitRevision.h"
