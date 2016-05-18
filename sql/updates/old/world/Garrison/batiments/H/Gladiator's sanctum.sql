990fdd8d161061d166a7d170f7d0d64cf855359a
Jeyel <contact.jl@voila.fr>
2016-05-18 15:24:33 +0200

    Database/Garrison : Forgot horde spawns

--- sql/updates/old/world/Garrison/batiments/H/Gladiator's sanctum.sql
+++ sql/updates/old/world/Garrison/batiments/H/Gladiator's sanctum.sql
@@ -6,3 +6,7 @@
 	REPLACE INTO creature_questender VALUE (86629, 36874);
 	
 	DELETE FROM garrison_plot_content WHERE plot_type_or_building = -159 AND faction_index = 0;
+	insert into `garrison_plot_content` (`plot_type_or_building`, `faction_index`, `creature_or_gob`, `x`, `y`, `z`, `o`) values('-159','0','86629','4.04582','-1.19722','1.76241','0.209029');
+	insert into `garrison_plot_content` (`plot_type_or_building`, `faction_index`, `creature_or_gob`, `x`, `y`, `z`, `o`) values('-159','0','-236744','5.242','4.2493','0.541428','5.88353');
+	insert into `garrison_plot_content` (`plot_type_or_building`, `faction_index`, `creature_or_gob`, `x`, `y`, `z`, `o`) values('-159','0','-236721','5.242','4.2493','1.55293','5.82463');
+	insert into `garrison_plot_content` (`plot_type_or_building`, `faction_index`, `creature_or_gob`, `x`, `y`, `z`, `o`) values('-159','0','86639','4.37512','-0.176027','1.08931','0.008749');
