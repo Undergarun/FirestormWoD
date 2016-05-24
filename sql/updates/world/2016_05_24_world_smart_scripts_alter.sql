930dea4318b73c733a1a85472d6668dac90c5f97
Jeyel <contact.jl@voila.fr>
2016-05-24 13:48:02 +0200

    Database/SAI : forgotten

--- sql/updates/world/2016_05_24_world_smart_scripts_alter.sql
+++ sql/updates/world/2016_05_24_world_smart_scripts_alter.sql
@@ -1,2 +1,2 @@
 alter table `623_world`.`smart_scripts` 
-   change `action_type` `action_type` smallint(5) UNSIGNED default '0' NOT NULL
+   change `action_type` `action_type` smallint(5) UNSIGNED default '0' NOT NULL;
