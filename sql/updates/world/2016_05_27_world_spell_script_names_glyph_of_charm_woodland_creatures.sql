490dfe37d5d688cee6ee98a069b2708fc7528d39
Zhalfyr <armelouduran@gmail.com>
2016-05-27 13:30:14 +0200

    Spells/Druid: Charm woodland creature is now link to the good glyph

---
+++ sql/updates/world/2016_05_27_world_spell_script_names_glyph_of_charm_woodland_creatures.sql
@@ -0,0 +1,2 @@
+DELETE FROM spell_script_names WHERE ScriptName = "spell_dru_glyph_of_charm_woodland_creature"; 
+INSERT INTO spell_script_names VALUE (57855, "spell_dru_glyph_of_charm_woodland_creature");
