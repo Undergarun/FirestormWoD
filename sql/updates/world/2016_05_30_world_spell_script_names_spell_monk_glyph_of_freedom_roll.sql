858a9c72dcab3cfd029eef36c4f2cce99e021411
Zhalfyr <armelouduran@gmail.com>
2016-05-30 16:19:44 +0200

    Spell/Monk: Glyph of Freedom roll
    Roll now remove impairing effects before doing the animation.

---
+++ sql/updates/world/2016_05_30_world_spell_script_names_spell_monk_glyph_of_freedom_roll.sql
@@ -0,0 +1,2 @@
+DELETE FROM spell_script_names WHERE spell_id = 107427 AND ScriptName = "spell_monk_glyph_of_freedom_roll";
+INSERT INTO spell_script_names VALUE (109132, "spell_monk_glyph_of_freedom_roll");
