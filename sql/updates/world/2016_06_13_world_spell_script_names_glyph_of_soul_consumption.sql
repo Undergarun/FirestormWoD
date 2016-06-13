c03186bfc7bafdcc3d018342acc3eae63b272591
pinkmouse <justin.mariette@gmail.com>
2016-06-13 15:12:22 +0200

    Spells/Warlock: Fix Glyph of soul consumption spell ID

---
+++ sql/updates/world/2016_06_13_world_spell_script_names_glyph_of_soul_consumption.sql
@@ -0,0 +1,2 @@
+DELETE FROM spell_script_names WHERE spell_id = 58070 AND ScriptName = "spell_warl_glyph_of_soul_consumption";
+INSERT INTO spell_script_names VALUE (58070, "spell_warl_glyph_of_soul_consumption");
