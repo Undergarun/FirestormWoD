141346f600785a8df7e25e810302f5795d52a2e6
Morgoporc <j.fraisse45@laposte.net>
2016-06-14 17:32:00 +0200

    Scripts/Toys: Implement Toy Train Set and Wind-Up Train Wrecker * And handle Thogar achievement
    Signed-off-by: Morgoporc <j.fraisse45@laposte.net>

--- src/server/game/DataStores/DBCStores.h
+++ src/server/game/DataStores/DBCStores.h
@@ -16,6 +16,8 @@
 typedef std::list<uint32> SimpleFactionsList;
 SimpleFactionsList const* GetFactionTeamList(uint32 faction);
 
+EmotesTextSoundEntry const* FindTextSoundEmoteFor(uint32 p_Emote, uint32 p_Race, uint32 p_Gender);
+
 char const* GetPetName(uint32 petfamily, uint32 dbclang);
 SpellEffectEntry const* GetSpellEffectEntry(uint32 spellId, uint32 effect, uint32 difficulty);
 SpellEffectScalingEntry const* GetSpellEffectScalingEntry(uint32 effectId);
