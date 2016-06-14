c60fbd5d6e1ead0b11994b0117e94be105a011b4
merydwin <merydwin@gmail.com>
2016-06-14 10:43:43 +0200

    Fixs SQL

--- sql/updates/world/2016_06_10_world_spell_script_names.sql
+++ sql/updates/world/2016_06_10_world_spell_script_names.sql
@@ -1,1 +1,2 @@
+DELETE FROM spell_script_names WHERE spell_id = 146939 AND ScriptName = "spell_item_enduring_elixir_of_wisdom";
 INSERT INTO `spell_script_names`(`spell_id`,`ScriptName`) VALUES ( '146939','spell_item_enduring_elixir_of_wisdom'); 
