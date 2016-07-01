e65bad0f32aef8ef4efad895d2809264602efee4
Zhalfyr <armelouduran@gmail.com>
2016-07-01 12:16:19 +0200

    Event/Midsummer: Achievement playerscript
    Fixed, now correctly handle Midsummer event achievment

--- src/server/game/Scripting/Interfaces/Interface_Player.hpp
+++ src/server/game/Scripting/Interfaces/Interface_Player.hpp
@@ -707,11 +707,13 @@ class PlayerScript : public ScriptObjectImpl<false>
         /// @p_Player : Player instance
         /// @p_Attacker  : Achievement
         /// @p_SendAchievement : Set to false to cancel achievement earned
-        virtual void OnAchievementEarned(Player* p_Player, AchievementEntry const* p_Achievement, bool& p_SendAchievement)
+        /// @p_After : True when the hook is after achievement earned, else : false
+        virtual void OnAchievementEarned(Player* p_Player, AchievementEntry const* p_Achievement, bool& p_SendAchievement, bool p_After)
         {
             UNUSED(p_Player);
             UNUSED(p_Achievement);
             UNUSED(p_SendAchievement);
+            UNUSED(p_After);
         }
 };
 
