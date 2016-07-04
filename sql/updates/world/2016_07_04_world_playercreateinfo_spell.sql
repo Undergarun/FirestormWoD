9384a7da414b30e6e41b4fc6de1df225e840397f
pinkmouse <justin.mariette@gmail.com>
2016-07-04 16:53:51 +0200

    Core/Spells: Fix Goblin to get Pack Hobgoblin racial

---
+++ sql/updates/world/2016_07_04_world_playercreateinfo_spell.sql
@@ -0,0 +1,8 @@
+INSERT INTO `playercreateinfo_spell` (race, class, spell, note) VALUES
+(9, 1, 69046, "Pack Hobgoblin"),
+(9, 3, 69046, "Pack Hobgoblin"),
+(9, 4, 69046, "Pack Hobgoblin"),
+(9, 5, 69046, "Pack Hobgoblin"),
+(9, 7, 69046, "Pack Hobgoblin"),
+(9, 8, 69046, "Pack Hobgoblin"),
+(9, 9, 69046, "Pack Hobgoblin");
